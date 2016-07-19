53abcf84ca14e3ae50631f0489f3a91e205c10ef
timelorduk <sjames8888@gmail.com>
2016-07-19 21:27:11 +0100

    1. add querytimeout  to cleaned up test suites - limit carefully connection open / close

--- reptest.bat
+++ reptest.bat
@@ -1,5 +1,5 @@
 echo start > ./test.txt
 for /L %%n in (1,1,100) do (
-	node runtest.js -t connect.js -t bulk.js -t query.js -t sproc.js >> ./test.txt 2>&1
+	node runtest.js -t querytimeout.js -t connect.js -t bulk.js -t query.js -t sproc.js >> ./test.txt 2>&1
 	echo "next " >> ./test.txt 2>&1
 )
--- test/querytimeout.js
+++ test/querytimeout.js
@@ -1,76 +1,74 @@
 var sql = require('../'),
     assert = require('assert'),
-    async = require('async'),
-    config = require('./test-config'),
+    supp = require('../demo-support'),
     fs = require('fs');
-
-var conn_str = config.conn_str;
 
 suite('querytimeout', function () {
 
-    var c;
     this.timeout(20 * 1000);
 
+    var theConnection;
+    var conn_str;
+    var support;
+    var async;
+    var helper;
+
     setup(function (test_done) {
-        test_done();
+        supp.GlobalConn.init(sql, function (co) {
+            conn_str = co.conn_str;
+            support = co.support;
+            async = co.async;
+            helper = co.helper;
+            helper.setVerbose(false);
+            sql.open(conn_str, function (err, new_conn) {
+                assert.ifError(err);
+                theConnection = new_conn;
+                test_done();
+            });
+        })
     });
 
     teardown(function (done) {
-        done();
-    });
-
-    var open = function (done) {
-        sql.open(conn_str, function (err, conn) {
-            if (err) {
-                console.error(err);
-                process.exit();
-            }
-
-            done(conn);
-        });
-    };
-
-    test('test timeout 2 secs on waitfor delay 10', function(test_done){
-        open(function(conn) {
-            var queryObj = {
-                query_str : "waitfor delay \'00:00:10\';",
-                query_timeout : 2
-            };
-
-            conn.query(queryObj, function (err, res) {
-                assert(err != null);
-                assert(err.message.indexOf('Query timeout expired') > 0)
-                test_done();
-            });
+        theConnection.close(function (err) {
+            assert.ifError(err);
+            done();
         });
     });
 
-    test('test timeout 10 secs on waitfor delay 2', function(test_done){
-        open(function(conn) {
-            var queryObj = {
-                query_str : "waitfor delay \'00:00:2\';",
-                query_timeout : 10
-            };
+    test('test timeout 2 secs on waitfor delay 10', function (test_done) {
+        var queryObj = {
+            query_str : "waitfor delay \'00:00:10\';",
+            query_timeout : 2
+        };
 
-            conn.query(queryObj, function (err, res) {
-                assert(err === null);
-                test_done();
-            });
+        theConnection.query(queryObj, function (err) {
+            assert(err != null);
+            assert(err.message.indexOf('Query timeout expired') > 0);
+            test_done();
         });
     });
 
-    test('test timeout 0 secs on waitfor delay 4', function(test_done){
+    test('test timeout 10 secs on waitfor delay 2', function (test_done) {
+        var queryObj = {
+            query_str : "waitfor delay \'00:00:2\';",
+            query_timeout : 10
+        };
 
-        open(function(conn) {
-            var queryObj = {
-                query_str : "waitfor delay \'00:00:4\';",
-                query_timeout : 0
-            };
+        theConnection.query(queryObj, function (err) {
+            assert.ifError(err);
+            test_done();
+        });
+    });
 
-            conn.query(queryObj, function (err, res) {
-                assert(err === null);
-                test_done();
-            });
+    test('test timeout 0 secs on waitfor delay 4', function (test_done) {
+
+        var queryObj = {
+            query_str: "waitfor delay \'00:00:4\';"
+        };
+
+        theConnection.query(queryObj, function (err) {
+            assert.ifError(err);
+            test_done();
         });
     });
 });
