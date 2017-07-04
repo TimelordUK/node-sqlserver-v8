82e9e82a8cb75b61102a6b51266ce64493917fda
Stephen James <sjames8888@gmail.com>
2017-07-04 21:21:32 +0100

    include query cancel in rep tests for soak testing driver

--- tool/reptest.bat
+++ tool/reptest.bat
@@ -5,6 +5,6 @@ rem FOR %%A IN (prepared.js params.js query.js querytimeout.js connect.js bulk.j
 rem         node runtest.js -t %%A >> ./test.txt 2>&1
 rem    )
     rem node runtest -t txn.js -t datatypes.js -t params.js -t query.js -t querytimeout.js -t connect.js -t bulk.js -t sproc.js -t prepared.js -t userbind.js -t dates.js >> ./test.txt 2>&1
-    node runtest -t txn.js -t datatypes.js -t params.js -t query.js -t querytimeout.js -t connect.js -t bulk.js -t sproc.js -t prepared.js -t userbind.js -t dates.js >> ./test.txt 2>&1
+    node runtest -t querycancel.js -t txn.js -t datatypes.js -t params.js -t query.js -t querytimeout.js -t connect.js -t bulk.js -t sproc.js -t prepared.js -t userbind.js -t dates.js >> ./test.txt 2>&1
 	echo "next " >> ./test.txt 2>&1
 )
