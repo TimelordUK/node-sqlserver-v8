debccb562cbda219055beb0f17ec6566a0f49ed6
Stephen James <sjames8888@gmail.com>
2017-09-10 21:58:49 +0100

    lint clean of sproc

--- test/sproc.js
+++ test/sproc.js
@@ -1,34 +1,32 @@
 var assert = require('assert')
 var supp = require('../demo-support')
 
+/* global suite teardown teardown test setup */
+
 suite('sproc', function () {
-  var conn_str
+  var connStr
   var theConnection
   var support
   var async
   var helper
-  var driver
-  var database
   var procedureHelper
   var sql = global.native_sql
 
   this.timeout(20000)
 
-  setup(function (test_done) {
+  setup(function (testDone) {
     supp.GlobalConn.init(sql, function (co) {
-      conn_str = co.conn_str
+      connStr = co.conn_str
       support = co.support
-      procedureHelper = new support.ProcedureHelper(conn_str)
+      procedureHelper = new support.ProcedureHelper(connStr)
       procedureHelper.setVerbose(false)
       async = co.async
       helper = co.helper
-      driver = co.driver
-      database = co.database
       helper.setVerbose(false)
-      sql.open(conn_str, function (err, conn) {
+      sql.open(connStr, function (err, conn) {
         theConnection = conn
         assert.ifError(err)
-        test_done()
+        testDone()
       })
     })
   })
@@ -40,9 +38,8 @@ suite('sproc', function () {
     })
   })
 
-  test('get proc and call  - should not error', function (test_done) {
-
-    var sp_name = 'test_sp_get_int_int'
+  test('get proc and call  - should not error', function (testDone) {
+    var spName = 'test_sp_get_int_int'
 
     var def = 'alter PROCEDURE <name>' +
       '(\n' +
@@ -58,14 +55,14 @@ suite('sproc', function () {
 
     var fns = [
       function (asyncDone) {
-        procedureHelper.createProcedure(sp_name, def, function () {
+        procedureHelper.createProcedure(spName, def, function () {
           asyncDone()
         })
       },
 
       function (asyncDone) {
         var pm = theConnection.procedureMgr()
-        pm.get(sp_name, function (proc) {
+        pm.get(spName, function (proc) {
           var count = pm.getCount()
           assert.equal(count, 1)
           proc.call([10, 5], function (err, results, output) {
@@ -79,13 +76,12 @@ suite('sproc', function () {
     ]
 
     async.series(fns, function () {
-      test_done()
+      testDone()
     })
   })
 
-  test('call proc that waits for delay of input param - wait 2, timeout 5 - should not error', function (test_done) {
-
-    var sp_name = 'test_spwait_for'
+  test('call proc that waits for delay of input param - wait 2, timeout 5 - should not error', function (testDone) {
+    var spName = 'test_spwait_for'
 
     var def = 'alter PROCEDURE <name>' +
       '(\n' +
@@ -98,7 +94,7 @@ suite('sproc', function () {
 
     var fns = [
       function (asyncDone) {
-        procedureHelper.createProcedure(sp_name, def, function () {
+        procedureHelper.createProcedure(spName, def, function () {
           asyncDone()
         })
       },
@@ -106,7 +102,7 @@ suite('sproc', function () {
       function (asyncDone) {
         var pm = theConnection.procedureMgr()
         pm.setTimeout(5)
-        pm.callproc(sp_name, ['0:0:2'], function (err) {
+        pm.callproc(spName, ['0:0:2'], function (err) {
           assert.ifError(err)
           asyncDone()
         })
@@ -114,36 +110,36 @@ suite('sproc', function () {
     ]
 
     async.series(fns, function () {
-      test_done()
+      testDone()
     })
   })
 
-  test('call proc that returns length of input string and decribes itself in results', function (test_done) {
+  test('call proc that returns length of input string and decribes itself in results', function (testDone) {
+    var spName = 'test_sp'
 
-    var sp_name = 'test_sp'
-
-    var def = 'alter PROCEDURE <name> @param VARCHAR(50) \n'
-      + ' AS \n'
-      + ' BEGIN \n'
-      + '     SELECT name, type, type_desc  FROM sys.objects WHERE type = \'P\' AND name = \'<name>\''
-      + '     RETURN LEN(@param); \n'
-      + ' END \n'
+    var def = 'alter PROCEDURE <name> @param VARCHAR(50) \n' +
+      ' AS \n' +
+      ' BEGIN \n' +
+      '     SELECT name, type, type_desc  FROM sys.objects WHERE type = \'P\' AND name = \'<name>\'' +
+      '     RETURN LEN(@param); \n' +
+      ' END \n'
 
     var fns = [
       function (asyncDone) {
-        procedureHelper.createProcedure(sp_name, def, function () {
+        procedureHelper.createProcedure(spName, def, function () {
           asyncDone()
         })
       },
 
       function (asyncDone) {
         var pm = theConnection.procedureMgr()
-        pm.callproc(sp_name, ['US of A!'], function (err, results, output) {
+        pm.callproc(spName, ['US of A!'], function (err, results, output) {
+          assert.ifError(err)
           var expected = [8]
           assert.deepEqual(output, expected, 'results didn\'t match')
           expected = [
             {
-              name: sp_name,
+              name: spName,
               type: 'P ',
               type_desc: 'SQL_STORED_PROCEDURE'
             }]
@@ -154,30 +150,30 @@ suite('sproc', function () {
     ]
 
     async.series(fns, function () {
-      test_done()
+      testDone()
     })
   })
 
-  test('call proc that returns length of input string', function (test_done) {
+  test('call proc that returns length of input string', function (testDone) {
+    var spName = 'test_sp'
 
-    var sp_name = 'test_sp'
-
-    var def = 'alter PROCEDURE <name> @param VARCHAR(50) \n'
-      + ' AS \n'
-      + ' BEGIN \n'
-      + '     RETURN LEN(@param); \n'
-      + ' END \n'
+    var def = 'alter PROCEDURE <name> @param VARCHAR(50) \n' +
+      ' AS \n' +
+      ' BEGIN \n' +
+      '     RETURN LEN(@param); \n' +
+      ' END \n'
 
     var fns = [
       function (asyncDone) {
-        procedureHelper.createProcedure(sp_name, def, function () {
+        procedureHelper.createProcedure(spName, def, function () {
           asyncDone()
         })
       },
 
       function (asyncDone) {
         var pm = theConnection.procedureMgr()
-        pm.callproc(sp_name, ['US of A!'], function (err, results, output) {
+        pm.callproc(spName, ['US of A!'], function (err, results, output) {
+          assert.ifError(err)
           var expected = [8]
           assert.deepEqual(output, expected, 'results didn\'t match')
           asyncDone()
@@ -186,13 +182,12 @@ suite('sproc', function () {
     ]
 
     async.series(fns, function () {
-      test_done()
+      testDone()
     })
   })
 
-  test('call proc that has 2 output string params + return code', function (test_done) {
-
-    var sp_name = 'test_sp_get_str_str'
+  test('call proc that has 2 output string params + return code', function (testDone) {
+    var spName = 'test_sp_get_str_str'
 
     var def = 'alter PROCEDURE <name>' +
       '(\n' +
@@ -209,14 +204,15 @@ suite('sproc', function () {
 
     var fns = [
       function (asyncDone) {
-        procedureHelper.createProcedure(sp_name, def, function () {
+        procedureHelper.createProcedure(spName, def, function () {
           asyncDone()
         })
       },
 
       function (asyncDone) {
         var pm = theConnection.procedureMgr()
-        pm.callproc(sp_name, [1], function (err, results, output) {
+        pm.callproc(spName, [1], function (err, results, output) {
+          assert.ifError(err)
           var expected = [99, 'name', 'company']
           assert.deepEqual(output, expected, 'results didn\'t match')
           asyncDone()
@@ -225,13 +221,12 @@ suite('sproc', function () {
     ]
 
     async.series(fns, function () {
-      test_done()
+      testDone()
     })
   })
 
-  test('call proc that has 2 input params + 1 output', function (test_done) {
-
-    var sp_name = 'test_sp_get_int_int'
+  test('call proc that has 2 input params + 1 output', function (testDone) {
+    var spName = 'test_sp_get_int_int'
 
     var def = 'alter PROCEDURE <name>' +
       '(\n' +
@@ -247,14 +242,15 @@ suite('sproc', function () {
 
     var fns = [
       function (asyncDone) {
-        procedureHelper.createProcedure(sp_name, def, function () {
+        procedureHelper.createProcedure(spName, def, function () {
           asyncDone()
         })
       },
 
       function (asyncDone) {
         var pm = theConnection.procedureMgr()
-        pm.callproc(sp_name, [10, 5], function (err, results, output) {
+        pm.callproc(spName, [10, 5], function (err, results, output) {
+          assert.ifError(err)
           var expected = [99, 15]
           assert.deepEqual(output, expected, 'results didn\'t match')
           asyncDone()
@@ -263,7 +259,7 @@ suite('sproc', function () {
     ]
 
     async.series(fns, function () {
-      test_done()
+      testDone()
     })
   })
 })
