7d78b838059f8686842d8f1334a8a0c1f2e21dbe
Stephen James <sjames8888@gmail.com>
2017-09-23 23:25:46 +0100

    try to model an employee record as a TVP

--- test/tvp.js
+++ test/tvp.js
@@ -34,6 +34,74 @@ suite('tvp', function () {
     })
   })
 
+  //
+
+  function populateTable(vec, table) {
+    vec.forEach(function(v) {
+      var row = []
+      table.columns.forEach(function (col) {
+        row.push(v[col.name])
+      })
+      table.rows.push(row)
+    })
+  }
+
+  test('use tvp to select from table type', function (testDone) {
+    var tableName = 'Employee'
+    var bulkMgr
+
+    var fns = [
+
+      function (asyncDone) {
+        helper.dropCreateTable({
+          name: tableName
+        }, function () {
+          asyncDone()
+        })
+      },
+
+      function (asyncDone) {
+        var tm = theConnection.tableMgr()
+        tm.bind(tableName, function (bulk) {
+          bulkMgr = bulk
+          asyncDone()
+        })
+      },
+
+      function (asyncDone) {
+        var sql = 'IF TYPE_ID(N\'EmployeeType\') IS not NULL'
+        sql += ' drop type EmployeeType'
+        theConnection.query(sql, function (err) {
+          //assert.ifError(err)
+          asyncDone()
+        })
+      },
+
+      function (asyncDone) {
+        var sql = bulkMgr.asUserType()
+        theConnection.query(sql, function (err) {
+          //assert.ifError(err)
+          asyncDone()
+        })
+      },
+
+      function (asyncDone) {
+        var parsedJSON = helper.getJSON()
+        var table = bulkMgr.asTableType()
+        populateTable(parsedJSON, table)
+        var q = 'exec EmployeeTvpSelect ( @tvp = ? )'
+        var tp = sql.Tvp(table.name, table)
+        theConnection.query(q, [tp], function(err, res) {
+          asyncDone()
+        })
+      }
+    ]
+
+    async.series(fns, function () {
+      testDone()
+    })
+  })
+
   test('employee use tm to get a table value type representing table and create that user table type', function (testDone) {
     var tableName = 'Employee'
     var bulkMgr
@@ -79,6 +147,8 @@ suite('tvp', function () {
           assert.ifError(err)
           var summary = bulkMgr.getSummary()
           assert(def.length = summary.columns.length)
+          var t = bulkMgr.asTableType()
+          assert(t.columns.length === summary.columns.length)
           asyncDone()
         })
       }
@@ -88,4 +158,5 @@ suite('tvp', function () {
       testDone()
     })
   })
+
 })
