e57d2f2a5f4be5841fb87a064a57118a53a6da33
Stephen James <sjames8888@gmail.com>
2018-03-03 22:24:43 +0000

    add a geography test - extend the max length of an error string to fit a .net error exception

--- unit.tests/geography.js
+++ unit.tests/geography.js
@@ -33,24 +33,7 @@ suite('geography', function () {
       done()
     })
   })
-  test('prepare a geography statement for repeat invocations', function (testDone) {
-    var preparedPoint = null
-    var points = [
-      'POINT (-89.349 -55.349)',
-      'POINT (1.349 -9.349)'
-    ]
-
-    var expected = [
-      {
-        id: 1,
-        GeogCol2: points[0]
-      },
-      {
-        id: 2,
-        GeogCol2: points[1]
-      }
-    ]
-
+  function createGeographyTable (done) {
     var fns = [
 
       function (asyncDone) {
@@ -63,10 +46,72 @@ suite('geography', function () {
           assert.ifError(e)
           asyncDone()
         })
+      }
+    ]
+    async.series(fns, function () {
+      done()
+    })
+  }
+
+  var points = [
+    'POINT (-89.349 -55.349)',
+    'POINT (1.349 -9.349)'
+  ]
+
+  var insertSql = 'INSERT INTO spatial_test (GeogCol1) VALUES (geography::STPointFromText(?, 4326))'
+  var selectSql = 'select id, GeogCol2 from spatial_test'
+
+  var expected = [
+    {
+      id: 1,
+      GeogCol2: points[0]
+    },
+    {
+      id: 2,
+      GeogCol2: points[1]
+    }
+  ]
+  test('insert an array of geography points', function (testDone) {
+    var fns = [
+
+      function (asyncDone) {
+        createGeographyTable(function () {
+          asyncDone()
+        })
       },
       function (asyncDone) {
-        var sql = 'INSERT INTO spatial_test (GeogCol1) VALUES (geography::STPointFromText(?, 4326))'
-        theConnection.prepare(sql, function (err, prepared) {
+        theConnection.query(insertSql, [points], function (err, res) {
+          assert.ifError(err)
+          assert(res.length === 0)
+          asyncDone()
+        })
+      },
+      function (asyncDone) {
+        theConnection.query(selectSql, function (err, res) {
+          assert.ifError(err)
+          assert(res.length === expected.length)
+          assert.deepEqual(res, expected)
+          asyncDone()
+        })
+      }
+    ]
+    async.series(fns, function () {
+      testDone()
+    })
+  })
+
+  test('prepare a geography statement for repeat invocations', function (testDone) {
+    var preparedPoint = null
+
+    var fns = [
+
+      function (asyncDone) {
+        createGeographyTable(function () {
+          asyncDone()
+        })
+      },
+      function (asyncDone) {
+        theConnection.prepare(insertSql, function (err, prepared) {
           assert.ifError(err)
           preparedPoint = prepared
           asyncDone()
@@ -87,9 +132,9 @@ suite('geography', function () {
         })
       },
       function (asyncDone) {
-        theConnection.query('select id, GeogCol2 from spatial_test', function (err, res) {
+        theConnection.query(selectSql, function (err, res) {
           assert.ifError(err)
-          assert(res.length === 2)
+          assert(res.length === expected.length)
           assert.deepEqual(res, expected)
           asyncDone()
         })
