64609d2a73970bc86888cec142f86619679c4507
Stephen <sjames8888@gmail.com>
2019-04-24 21:54:24 +0100

    lint JS files with newer JS syntax

--- unit.tests/userbind.js
+++ unit.tests/userbind.js
@@ -13,13 +13,13 @@ suite('userbind', function () {
 
   const sql = global.native_sql
 
-  setup(function (testDone) {
-    supp.GlobalConn.init(sql, function (co) {
+  setup( testDone => {
+    supp.GlobalConn.init(sql, co => {
       connStr = global.conn_str || co.conn_str
       async = co.async
       helper = co.helper
       helper.setVerbose(false)
-      sql.open(connStr, function (err, newConn) {
+      sql.open(connStr, (err, newConn) => {
         assert(err === false)
         theConnection = newConn
         testDone()
@@ -27,8 +27,8 @@ suite('userbind', function () {
     }, global.conn_str)
   })
 
-  teardown(function (done) {
-    theConnection.close(function () {
+  teardown(done => {
+    theConnection.close(() => {
       done()
     })
   })
@@ -40,8 +40,8 @@ suite('userbind', function () {
 
     const sequence = [
 
-      function (asyncDone) {
-        theConnection.query(params.query, [params.setter(params.min)], function (err, res) {
+      asyncDone => {
+        theConnection.query(params.query, [params.setter(params.min)], (err, res) => {
           error = err
           if (err) {
             skip = true
@@ -53,12 +53,12 @@ suite('userbind', function () {
         })
       },
 
-      function (asyncDone) {
+      asyncDone => {
         if (skip) {
           asyncDone()
           return
         }
-        theConnection.query(params.query, [params.setter(params.max)], function (err, res) {
+        theConnection.query(params.query, [params.setter(params.max)], (err, res) => {
           error = err
           if (err) {
             skip = true
@@ -70,7 +70,7 @@ suite('userbind', function () {
         })
       },
 
-      function (asyncDone) {
+      asyncDone => {
         if (skip) {
           asyncDone()
           return
@@ -80,7 +80,7 @@ suite('userbind', function () {
             asyncDone()
           }
         } else {
-          theConnection.query(params.query, [params.setter(null)], function (err, res) {
+          theConnection.query(params.query, [params.setter(null)], (err, res) => {
             error = err
             if (err) {
               error = err
@@ -95,7 +95,7 @@ suite('userbind', function () {
     ]
 
     async.series(sequence,
-      function () {
+      () => {
         cb(error, allres)
       })
   }
@@ -122,23 +122,23 @@ suite('userbind', function () {
     assert.deepEqual(res, expected)
   }
 
-  test('user bind DateTime2 to sql type datetime2(7) - with scale set to illegal value, should error', function (testDone) {
+  test('user bind DateTime2 to sql type datetime2(7) - with scale set to illegal value, should error', testDone => {
     const now = new Date()
     const params = {
       query: 'declare @v DATETIME2(7) = ?; select @v as v',
       min: now,
       max: now,
-      setter: function (v) {
+      setter: v => {
         return sql.DateTime2(v, 8) // set scale illegal
       }
     }
-    testUserBind(params, function (err) {
+    testUserBind(params, err => {
       assert(err)
       testDone()
     })
   })
 
-  test('user bind DateTime2 to sql type datetime2(7) - with scale set too low, should error', function (testDone) {
+  test('user bind DateTime2 to sql type datetime2(7) - with scale set too low, should error', testDone => {
     const jsonDate = '2011-05-26T07:56:00.123Z'
     const then = new Date(jsonDate)
     const params = {
@@ -159,7 +159,7 @@ suite('userbind', function () {
     return new Array(num + 1).join(a)
   }
 
-  test('user bind WLongVarChar to NVARCHAR(MAX)', function (testDone) {
+  test('user bind WLongVarChar to NVARCHAR(MAX)', testDone => {
     const smallLen = 2200
     const largeLen = 8200
     const params = {
@@ -167,18 +167,18 @@ suite('userbind', function () {
       min: repeat('N', smallLen),
       max: repeat('X', largeLen),
       test_null: false,
-      setter: function (v) {
+      setter: v => {
         return sql.WLongVarChar(v)
       }
     }
-    testUserBind(params, function (err, res) {
+    testUserBind(params, (err, res) => {
       assert.ifError(err)
       compare(params, res)
       testDone()
     })
   })
 
-  test('user bind DateTimeOffset to sql type DateTimeOffset - provide offset of 60 minutes', function (testDone) {
+  test('user bind DateTimeOffset to sql type DateTimeOffset - provide offset of 60 minutes', testDone => {
     const offset = 60
     const scale = 7
     const now = new Date()
@@ -200,7 +200,7 @@ suite('userbind', function () {
         expected,
         expected
       ],
-      setter: function (v) {
+      setter: v => {
         return sql.DateTimeOffset(v, scale, offset)
       }
     }
@@ -211,7 +211,7 @@ suite('userbind', function () {
     })
   })
 
-  test('user bind DateTimeOffset to sql type DateTimeOffset - no offset ', function (testDone) {
+  test('user bind DateTimeOffset to sql type DateTimeOffset - no offset ', testDone => {
     const now = new Date()
     const smalldt = new Date(Date.UTC(now.getUTCFullYear(),
       now.getUTCMonth(),
@@ -224,7 +224,7 @@ suite('userbind', function () {
       query: 'declare @v DateTimeOffset = ?; select @v as v',
       min: smalldt,
       max: smalldt,
-      setter: function (v) {
+      setter: v => {
         return sql.DateTimeOffset(v)
       }
     }
@@ -235,7 +235,7 @@ suite('userbind', function () {
     })
   })
 
-  test('user bind SmallDateTime to sql type smalldatetime', function (testDone) {
+  test('user bind SmallDateTime to sql type smalldatetime', testDone => {
     const now = new Date()
     const smalldt = new Date(Date.UTC(now.getUTCFullYear(),
       now.getUTCMonth(),
@@ -248,102 +248,102 @@ suite('userbind', function () {
       query: 'declare @v smalldatetime = ?; select @v as v',
       min: smalldt,
       max: smalldt,
-      setter: function (v) {
+      setter: v => {
         return sql.SmallDateTime(v)
       }
     }
-    testUserBind(params, function (err, res) {
+    testUserBind(params, (err, res) => {
       assert.ifError(err)
       compare(params, res)
       testDone()
     })
   })
 
-  test('user bind DateTime2 to sql type datetime2(7) default scale', function (testDone) {
+  test('user bind DateTime2 to sql type datetime2(7) default scale', testDone => {
     const now = new Date()
     const params = {
       query: 'declare @v DATETIME2(7) = ?; select @v as v',
       min: now,
       max: now,
-      setter: function (v) {
+      setter: v => {
         return sql.DateTime2(v)
       }
     }
-    testUserBind(params, function (err, res) {
+    testUserBind(params, (err, res) => {
       assert.ifError(err)
       compare(params, res)
       testDone()
     })
   })
 
-  test('user bind DateTime2 to sql type datetime2(7) - with scale set correctly, should pass', function (testDone) {
+  test('user bind DateTime2 to sql type datetime2(7) - with scale set correctly, should pass', testDone => {
     const now = new Date()
     const params = {
       query: 'declare @v DATETIME2(7) = ?; select @v as v',
       min: now,
       max: now,
-      setter: function (v) {
+      setter: v => {
         return sql.DateTime2(v, 3) // set scale just right for ms
       }
     }
-    testUserBind(params, function (err, res) {
+    testUserBind(params, (err, res) => {
       assert.ifError(err)
       compare(params, res)
       testDone()
     })
   })
 
-  test('user bind DateTime to sql type datetime2(7)', function (testDone) {
+  test('user bind DateTime to sql type datetime2(7)', testDone => {
     const now = new Date()
     const params = {
       query: 'declare @v DATETIME2(7) = ?; select @v as v',
       min: now,
       max: now,
-      setter: function (v) {
+      setter: v => {
         return sql.DateTime(v)
       }
     }
-    testUserBind(params, function (err, res) {
+    testUserBind(params,  (err, res) => {
       assert.ifError(err)
       compare(params, res)
       testDone()
     })
   })
 
-  test('user bind DateTime to sql type datetime - driver currently only supports 10ms accuracy with datetime', function (testDone) {
+  test('user bind DateTime to sql type datetime - driver currently only supports 10ms accuracy with datetime', testDone => {
     const now = sql.DateRound()
     const params = {
       query: 'declare @v DATETIME2(7) = ?; select @v as v',
       min: now,
       max: now,
-      setter: function (v) {
+      setter: v => {
         return sql.DateTime(v)
       }
     }
-    testUserBind(params, function (err, res) {
+    testUserBind(params, (err, res) => {
       assert.ifError(err)
       compare(params, res)
       testDone()
     })
   })
 
-  test('user bind UniqueIdentifier', function (testDone) {
+  test('user bind UniqueIdentifier', (testDone) => {
     const params = {
       query: 'declare @v uniqueidentifier = ?; select @v as v',
       min: 'F01251E5-96A3-448D-981E-0F99D789110D',
       max: '45E8F437-670D-4409-93CB-F9424A40D6EE',
-      setter: function (v) {
+      setter: v => {
         return sql.UniqueIdentifier(v)
       }
     }
-    testUserBind(params, function (err, res) {
+    testUserBind(params,  (err, res) => {
       assert.ifError(err)
       compare(params, res)
       testDone()
     })
   })
 
-  test('user bind Time', function (testDone) {
+  test('user bind Time', (testDone) => {
     const today = new Date()
     const timeOnly = new Date(Date.UTC(1900,
       0,
@@ -360,18 +360,18 @@ suite('userbind', function () {
         timeOnly,
         timeOnly
       ],
-      setter: function (v) {
+      setter: v => {
         return sql.Time(v)
       }
     }
-    testUserBind(params, function (err, res) {
+    testUserBind(params, (err, res) => {
       assert.ifError(err)
       compare(params, res)
       testDone()
     })
   })
 
-  test('user bind Date', function (testDone) {
+  test('user bind Date', testDone => {
     const today = new Date()
     const dateOnly = new Date(Date.UTC(today.getUTCFullYear(),
       today.getUTCMonth(),
@@ -388,49 +388,49 @@ suite('userbind', function () {
         dateOnly,
         dateOnly
       ],
-      setter: function (v) {
+      setter: v => {
         return sql.Date(v)
       }
     }
-    testUserBind(params, function (err, res) {
+    testUserBind(params, (err, res) => {
       assert.ifError(err)
       compare(params, res)
       testDone()
     })
   })
 
-  test('user bind Xml - well formatted.', function (testDone) {
+  test('user bind Xml - well formatted.', testDone => {
     const params = {
       query: 'declare @v xml = ?; select @v as v',
       min: '<Cars><Car id="1234"><Make>Volkswagen</Make><Model>Eurovan</Model><Year>2003</Year><Color>White</Color></Car></Cars>',
       max: '<Cars><Car id="1234"><Make>Volkswagen</Make><Model>Eurovan</Model><Year>2003</Year><Color>White</Color></Car><Car id="5678"><Make>Honda</Make><Model>CRV</Model><Year>2009</Year><Color>Black</Color><Mileage>35,600</Mileage></Car></Cars>',
-      setter: function (v) {
+      setter: v => {
         return sql.Xml(v)
       }
     }
-    testUserBind(params, function (err, res) {
+    testUserBind(params, (err, res) => {
       assert.ifError(err)
       compare(params, res)
       testDone()
     })
   })
 
-  test('user bind Xml - bad xml should give error', function (testDone) {
+  test('user bind Xml - bad xml should give error', (testDone) => {
     const params = {
       query: 'declare @v xml = ?; select @v as v',
       min: '',
       max: '<Cars><Car id="1234"><Make>Volkswagen</Make><Model>Eurovan</Model><Year>2003</Year><Color>White</Color></Cars>',
-      setter: function (v) {
+      setter: v => {
         return sql.Xml(v)
       }
     }
-    testUserBind(params, function (err) {
+    testUserBind(params, err => {
       assert.ok(err.message.indexOf('end tag does not match start tag') > 0)
       testDone()
     })
   })
 
-  test('user bind nchar - check truncated user strings (1)', function (testDone) {
+  test('user bind nchar - check truncated user strings (1)', testDone => {
     const params = {
       query: 'declare @v nchar(5) = ?; select @v as v',
       min: 'five',
@@ -440,18 +440,18 @@ suite('userbind', function () {
         'hello'
       ],
 
-      setter: function (v) {
+      setter: v => {
         return sql.NChar(v)
       }
     }
-    testUserBind(params, function (err, res) {
+    testUserBind(params, (err, res) => {
       assert.ifError(err)
       compare(params, res)
       testDone()
     })
   })
 
-  test('user bind Char - check truncated user strings (1)', function (testDone) {
+  test('user bind Char - check truncated user strings (1)', testDone => {
     const params = {
       query: 'declare @v char(5) = ?; select @v as v',
       min: 'five',
@@ -460,18 +460,18 @@ suite('userbind', function () {
         'five ',
         'hello'
       ],
-      setter: function (v) {
+      setter: v => {
         return sql.Char(v)
       }
     }
-    testUserBind(params, function (err, res) {
+    testUserBind(params, (err, res) => {
       assert.ifError(err)
       compare(params, res)
       testDone()
     })
   })
 
-  test('user bind Char - returned string will be padded (2)', function (testDone) {
+  test('user bind Char - returned string will be padded (2)', testDone => {
     const params = {
       query: 'declare @v char(5) = ?; select @v as v',
       min: 'h',
@@ -480,18 +480,18 @@ suite('userbind', function () {
         'h    ',
         'world'
       ],
-      setter: function (v) {
+      setter: v => {
         return sql.Char(v)
       }
     }
-    testUserBind(params, function (err, res) {
+    testUserBind(params, (err, res) => {
       assert.ifError(err)
       compare(params, res)
       testDone()
     })
   })
 
-  test('user bind Char - use precision to clip user string (3)', function (testDone) {
+  test('user bind Char - use precision to clip user string (3)', testDone => {
     const params = {
       query: 'declare @v char(11) = ?; select @v as v',
       min: 'h',
@@ -500,18 +500,18 @@ suite('userbind', function () {
         'h' + new Array(11).join(' '),
         'wo' + new Array(10).join(' ')
       ],
-      setter: function (v) {
+      setter: v => {
         return sql.Char(v, 2)
       }
     }
-    testUserBind(params, function (err, res) {
+    testUserBind(params, (err, res) => {
       assert.ifError(err)
       compare(params, res)
       testDone()
     })
   })
 
-  test('user bind NVarChar /16 bit encoded', function (testDone) {
+  test('user bind NVarChar /16 bit encoded', testDone => {
     const params = {
       query: 'declare @v varchar(100) = ?; select @v as v',
       min: 'hello',
@@ -520,123 +520,123 @@ suite('userbind', function () {
         'hello',
         'world'
       ],
-      setter: function (v) {
+      setter: v => {
         return sql.NVarChar(v)
       }
     }
-    testUserBind(params, function (err, res) {
+    testUserBind(params, (err, res) => {
       assert.ifError(err)
       compare(params, res)
       testDone()
     })
   })
 
-  test('user bind Float, maps to numeric data structure.', function (testDone) {
+  test('user bind Float, maps to numeric data structure.', (testDone) => {
     const params = {
       query: 'declare @v float = ?; select @v as v',
       min: -1.7976931348623158E+308,
       max: 1.7976931348623158E+308,
-      setter: function (v) {
+      setter: v => {
         return sql.Float(v)
       }
     }
-    testUserBind(params, function (err, res) {
+    testUserBind(params, (err, res) => {
       assert.ifError(err)
       compare(params, res)
       testDone()
     })
   })
 
-  test('user bind Double, maps to numeric data structure.', function (testDone) {
+  test('user bind Double, maps to numeric data structure.', testDone => {
     const params = {
       query: 'declare @v float = ?; select @v as v',
       min: -1.7976931348623158E+308,
       max: 1.7976931348623158E+308,
-      setter: function (v) {
+      setter: v => {
         return sql.Float(v)
       }
     }
-    testUserBind(params, function (err, res) {
+    testUserBind(params, (err, res) => {
       assert.ifError(err)
       compare(params, res)
       testDone()
     })
   })
 
-  test('user bind Bit', function (testDone) {
+  test('user bind Bit', testDone => {
     const params = {
       query: 'declare @v bit = ?; select @v as v',
       min: false,
       max: true,
-      setter: function (v) {
+      setter: v => {
         return sql.Bit(v)
       }
     }
-    testUserBind(params, function (err, res) {
+    testUserBind(params, (err, res) => {
       assert.ifError(err)
       compare(params, res)
       testDone()
     })
   })
 
-  test('user bind BigInt', function (testDone) {
+  test('user bind BigInt', testDone => {
     const params = {
       query: 'declare @v bigint = ?; select @v as v',
       min: -9007199254740991,
       max: 9007199254740991,
-      setter: function (v) {
+      setter: v => {
         return sql.BigInt(v)
       }
     }
-    testUserBind(params, function (err, res) {
+    testUserBind(params, (err, res) => {
       assert.ifError(err)
       compare(params, res)
       testDone()
     })
   })
 
-  test('user bind Int', function (testDone) {
+  test('user bind Int', (testDone) => {
     const params = {
       query: 'declare @v int = ?; select @v as v',
       min: -2147483648,
       max: 2147483647,
-      setter: function (v) {
+      setter: v => {
         return sql.Int(v)
       }
     }
-    testUserBind(params, function (err, res) {
+    testUserBind(params, (err, res) => {
       assert.ifError(err)
       compare(params, res)
       testDone()
     })
   })
 
-  test('user bind TinyInt', function (testDone) {
+  test('user bind TinyInt', testDone => {
     const params = {
       query: 'declare @v tinyint = ?; select @v as v',
       min: 0,
       max: 255,
-      setter: function (v) {
+      setter: v => {
         return sql.TinyInt(v)
       }
     }
-    testUserBind(params, function (err, res) {
+    testUserBind(params, (err, res) => {
       assert.ifError(err)
       compare(params, res)
       testDone()
     })
   })
 
-  test('user bind SmallInt', function (testDone) {
+  test('user bind SmallInt', testDone => {
     const params = {
       query: 'declare @v smallint = ?; select @v as v',
       min: -32768,
       max: 32767,
-      setter: function (v) {
+      setter: v => {
         return sql.SmallInt(v)
       }
     }
-    testUserBind(params, function (err, res) {
+    testUserBind(params, (err, res) => {
       assert.ifError(err)
       compare(params, res)
       testDone()
--- unit.tests/warnings.js
+++ unit.tests/warnings.js
@@ -2,22 +2,22 @@
 
 /* global suite test setup */
 
-var assert = require('assert')
-var supp = require('../samples/typescript/demo-support')
+const assert = require('assert')
+const supp = require('../samples/typescript/demo-support')
 
 suite('warnings', function () {
-  var connStr
-  var theConnection
-  var support
-  var async
-  var helper
-  var procedureHelper
-  var sql = global.native_sql
+  let connStr
+  let theConnection
+  let support
+  let async
+  let helper
+  let procedureHelper
+  const sql = global.native_sql
 
   this.timeout(20000)
 
-  setup(function (testDone) {
-    supp.GlobalConn.init(sql, function (co) {
+  setup(testDone => {
+    supp.GlobalConn.init(sql, co => {
       connStr = global.conn_str || co.conn_str
       support = co.support
       procedureHelper = new support.ProcedureHelper(connStr)
@@ -25,7 +25,7 @@ suite('warnings', function () {
       async = co.async
       helper = co.helper
       helper.setVerbose(false)
-      sql.open(connStr, function (err, conn) {
+      sql.open(connStr, (err, conn) => {
         theConnection = conn
         assert(err === false)
         testDone()
@@ -33,7 +33,7 @@ suite('warnings', function () {
     }, global.conn_str)
   })
 
-  var joinFailTestQry =
+  const joinFailTestQry =
     'SELECT tmp. * ' +
     'FROM (' +
     ' SELECT 1 [id], \'test1\' [value]' +
@@ -45,7 +45,7 @@ suite('warnings', function () {
     ') tmp2 ON tmp.id = tmp2.id2 ' +
     ' OPTION (RECOMPILE);'
 
-  var nullEliminatedTestQry =
+  const nullEliminatedTestQry =
     'SELECT ' +
     ' SUM(tmp.[A])' +
     ' FROM ( ' +
@@ -56,42 +56,42 @@ suite('warnings', function () {
     ' OPTION (RECOMPILE);'
 
   function testQry (qry, done) {
-    var errors = []
-    var warnings = []
-    var meta
-    var res = []
-    var obj
-    var stmt = theConnection.queryRaw(qry)
-    stmt.on('meta', function (m) {
+    const errors = []
+    const warnings = []
+    let meta
+    const res = []
+    let obj
+    const stmt = theConnection.queryRaw(qry)
+    stmt.on('meta', m => {
       meta = m
     })
-    stmt.on('error', function (err) {
+    stmt.on('error', err => {
       errors.push(err)
     })
-    stmt.on('info', function (err) {
+    stmt.on('info', err => {
       warnings.push(err)
     })
-    stmt.on('column', function (c, d) {
+    stmt.on('column', (c, d) => {
       obj.push(d)
     })
-    stmt.on('row', function () {
+    stmt.on('row', () => {
       obj = []
       res.push(obj)
     })
-    stmt.on('done', function () {
+    stmt.on('done', () => {
       done(warnings, errors, meta, res)
     })
   }
 
   function testPrepared (qry, done) {
-    var errors = []
-    var warnings = []
-    theConnection.prepare(qry, function (e, ps) {
-      ps.preparedQuery([1], function (err) {
+    const errors = []
+    const warnings = []
+    theConnection.prepare(qry, (e, ps) => {
+      ps.preparedQuery([1], (err) => {
         if (err) {
           errors.push(err)
         }
-        ps.free(function () {
+        ps.free(() => {
           done(warnings, errors)
         })
       })
@@ -99,16 +99,16 @@ suite('warnings', function () {
   }
 
   function testSP (done) {
-    var errors = []
-    var warnings = []
-    var pm = theConnection.procedureMgr()
-    var sp = pm.callproc('tstWarning')
+    const errors = []
+    const warnings = []
+    const pm = theConnection.procedureMgr()
+    const sp = pm.callproc('tstWarning')
 
-    sp.on('error', function (err) {
+    sp.on('error', (err) => {
       errors.push(err)
       done(warnings, errors)
     })
-    sp.on('info', function (err) {
+    sp.on('info', (err) => {
       warnings.push(err)
       done(warnings, errors)
     })
@@ -118,8 +118,8 @@ suite('warnings', function () {
     testSP(connStr, 'TEST FIVE - Stord Proc - JOIN HINT WARNING')
     */
 
-  test('TEST ONE - Query - JOIN HINT WARNING', function (testDone) {
-    var expected = [
+  test('TEST ONE - Query - JOIN HINT WARNING', testDone => {
+    const expected = [
       [
         1,
         'test1'
@@ -129,126 +129,126 @@ suite('warnings', function () {
         'test2'
       ]
     ]
-    var fns = [
-      function (asyncDone) {
-        testQry(joinFailTestQry, function (warnings, errors, meta, res) {
+    const fns = [
+      asyncDone => {
+        testQry(joinFailTestQry, (warnings, errors, meta, res) => {
           assert(meta)
           assert(meta.length === 2)
           assert(warnings.length === 1)
           assert(errors.length === 0)
-          assert.deepEqual(expected, res)
+          assert.deepStrictEqual(expected, res)
           asyncDone()
         })
       }
     ]
 
-    async.series(fns, function () {
+    async.series(fns, () => {
       testDone()
     })
   })
 
-  test('TEST TWO - Query - NULL ELIMNATED WARNING', function (testDone) {
-    var expected = [
+  test('TEST TWO - Query - NULL ELIMNATED WARNING', testDone => {
+    const expected = [
       [
         8
       ]
     ]
-    var fns = [
-      function (asyncDone) {
-        testQry(nullEliminatedTestQry, function (warnings, errors, meta, res) {
+    const fns = [
+      asyncDone => {
+        testQry(nullEliminatedTestQry, (warnings, errors, meta, res) => {
           assert(warnings.length === 0)
           assert(errors.length === 0)
           assert(meta)
-          assert.deepEqual(res, expected)
+          assert.deepStrictEqual(res, expected)
           assert(meta.length === 1)
           asyncDone()
         })
       }
     ]
 
-    async.series(fns, function () {
+    async.series(fns, () => {
       testDone()
     })
   })
 
-  test('TEST THREE - Prepared Query - JOIN HINT WARNING', function (testDone) {
-    var fns = [
-      function (asyncDone) {
-        testPrepared(joinFailTestQry, function (warnings, errors) {
+  test('TEST THREE - Prepared Query - JOIN HINT WARNING', testDone => {
+    const fns = [
+      asyncDone => {
+        testPrepared(joinFailTestQry, (warnings, errors) => {
           assert(errors.length === 0)
           asyncDone()
         })
       }
     ]
 
-    async.series(fns, function () {
+    async.series(fns, () => {
       testDone()
     })
   })
 
-  test('TEST FOUR - Prepared Query - NULL ELIMNATED WARNING', function (testDone) {
-    var fns = [
-      function (asyncDone) {
-        testPrepared(nullEliminatedTestQry, function (warnings, errors) {
+  test('TEST FOUR - Prepared Query - NULL ELIMNATED WARNING', testDone => {
+    const fns = [
+      asyncDone => {
+        testPrepared(nullEliminatedTestQry, (warnings, errors) => {
           assert(errors.length === 0)
           asyncDone()
         })
       }
     ]
 
-    async.series(fns, function () {
+    async.series(fns, () => {
       testDone()
     })
   })
 
-  test('TEST FIVE - Stord Proc - JOIN HINT WARNING', function (testDone) {
-    var fns = [
-      function (asyncDone) {
-        testSP(function (warnings, errors) {
+  test('TEST FIVE - Stord Proc - JOIN HINT WARNING', testDone => {
+    const fns = [
+      asyncDone => {
+        testSP((warnings, errors) => {
           assert(errors.length === 1)
           asyncDone()
         })
       }
     ]
 
-    async.series(fns, function () {
+    async.series(fns, () => {
       testDone()
     })
   })
 
-  test('print raises warning not error', function (testDone) {
-    var fns = [
+  test('print raises warning not error', testDone => {
+    const fns = [
       function (asyncDone) {
-        var warnings = []
-        var err = new Error('[Microsoft][SQL Server Native Client 11.0][SQL Server]print error')
+        const warnings = []
+        const err = new Error('[Microsoft][SQL Server Native Client 11.0][SQL Server]print error')
         err.code = 0
         err.sqlstate = '01000'
-        var expectedErrors = [err]
-        var expectedResults = [
+        const expectedErrors = [err]
+        const expectedResults = [
           {
             cnt: 1
           }
         ]
-        var sql = 'print \'print error\'; select 1 as cnt'
-        var q = theConnection.query(sql, [], function (err, res, more) {
+        const sql = 'print \'print error\'; select 1 as cnt'
+        const q = theConnection.query(sql, [], (err, res, more) => {
           assert.ifError(err)
           if (!more) {
             assert(warnings.length === 1)
-            assert.deepEqual(warnings, expectedErrors)
-            assert.deepEqual(res, expectedResults)
+            assert.deepStrictEqual(warnings, expectedErrors)
+            assert.deepStrictEqual(res, expectedResults)
             asyncDone()
           }
         })
-        q.on('error', function (err) {
+        q.on('error', err => {
           assert.ifError(err)
         })
-        q.on('info', function (err) {
+        q.on('info', err => {
           warnings.push(err)
         })
       }
     ]
 
-    async.series(fns, function () {
+    async.series(fns, () => {
       testDone()
     })
   })
