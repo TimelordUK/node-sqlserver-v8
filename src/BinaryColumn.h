5048dd1279a0f36ce7a1b4e83611aab2e47539c3
Stephen <sjames8888@gmail.com>
2020-03-15 16:54:40 +0000

    fix binary column memory management

--- src/BinaryColumn.h
+++ src/BinaryColumn.h
@@ -13,42 +13,14 @@ namespace mssql
     class BinaryColumn : public Column
     {
     public:
-		BinaryColumn(int id, shared_ptr<DatumStorage> storage, size_t l) : Column(id)
-			, len(l), offset(0), raw(clone(storage->charvec_ptr, offset, l))
-		{
-		}
-
-		BinaryColumn(int id, shared_ptr<DatumStorage> storage, size_t offset, size_t l) : Column(id)
-			, len(l), offset(offset), raw(clone(storage->charvec_ptr, offset, l))
-		{
-		}
-
-	   Local<Value> ToValue() override
-	   {
-		   return node::Buffer::New(Isolate::GetCurrent(), raw, len, deleteBuffer, raw)
-#ifdef NODE_GYP_V4 
-			   .ToLocalChecked()
-#endif
-			   ;
-	   }
-
+		BinaryColumn(const int id, shared_ptr<DatumStorage> s, size_t l);
+		BinaryColumn(const int id, shared_ptr<DatumStorage> s, size_t offset, size_t l);
+		Local<Value> ToValue() override;
     private:
 
-		static char *clone(shared_ptr<DatumStorage::char_vec_t> sp, size_t offset, size_t len)
-		{
-			char *dest = new char[len];
-			memcpy(dest, sp->data() + offset, len);
-			return dest;
-		}
-
-		static void deleteBuffer(char* ptr, void* hint)
-		{
-			//fprintf(stderr, "delete ptr %p\n", hint);
-			delete[] hint;
-		}
-
+		shared_ptr<DatumStorage::char_vec_t> storage;
 		size_t len;
 		size_t offset;
-		char *raw;
+
     };
 }
