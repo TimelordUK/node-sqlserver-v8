035be92fa002fce2a92a6193e82d0c295541b613
Stephen James <sjames8888@gmail.com>
2020-03-26 21:42:11 +0000

    some lint

--- samples/javascript/simple-http.js
+++ samples/javascript/simple-http.js
@@ -2,41 +2,22 @@ const http = require('http')
 const hostname = 'localhost'
 const port = 2020
 const sql = require('msnodesqlv8')
+const util = require('util')
 
 const connectionString = 'driver={SQL Server Native Client 11.0};server=(localdb)\\node;trusted_connection=yes;database=scratch;'
 const query = 'select top 50 object_name(c.object_id), (select dc.definition from sys.default_constraints as dc where dc.object_id = c.default_object_id) as DefaultValueExpression from sys.columns as c'
-
-function toPromise (f) {
-  return function (args) {
-    return new Promise((resolve, reject) => {
-      function handler (err, res) {
-        if (err) {
-          reject(err)
-        } else {
-          resolve(res)
-        }
-      }
-
-      if (args) {
-        f(args, handler)
-      } else {
-        f(handler)
-      }
-    })
-  }
-}
 
 async function test (request, response) {
   response.statusCode = 200
   response.setHeader('Content-Type', 'text/plain')
 
   console.log('>> test')
-  const sqlOpen = toPromise(sql.open)
+  const sqlOpen = util.promisify(sql.open)
   try {
     console.log('sqlOpen ....')
     const connection = await sqlOpen(connectionString)
     console.log('..... sqlOpen')
-    const connectionQuery = toPromise(connection.queryRaw)
+    const connectionQuery = util.promisify(connection.queryRaw)
     try {
       const d = new Date()
       console.log('connectionQuery 1 ....')
@@ -45,7 +26,7 @@ async function test (request, response) {
       const elapsed = new Date() - d
       console.log(`rows.length ${data.rows.length} elapsed ${elapsed}`)
       response.end(JSON.stringify(data, null, 4))
-      const close = toPromise(sql.close)
+      const close = util.promisify(sql.close)
       console.log('close ...')
       await close()
       console.log('... close')
