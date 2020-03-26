470dd9e23bcca1566f4dfb818e6f73b708dcc2e8
Stephen James <sjames8888@gmail.com>
2020-03-26 21:32:23 +0000

    some lint

--- samples/javascript/sequelize.js
+++ samples/javascript/sequelize.js
@@ -33,59 +33,56 @@ function createUserModel () {
   })
 }
 
-function userModel () {
-  return new Promise(async (resolve, reject) => {
-    const user = createUserModel()
-    // force: true will drop the table if it already exists
-    await user.sync({ force: true })
-    await Promise.all([
-      user.create({
-        username: 'techno01',
-        job: 'Programmer'
-      }),
-      user.create({
-        username: 'techno02',
-        job: 'Head Programmer'
-      }),
-      user.create({
-        username: 'techno03',
-        job: 'Agile Leader'
-      })
-    ]).catch((e) => reject(e))
-
-    const id1 = await user.findByPk(3)
-    console.log(JSON.stringify(id1, null, 4))
-
-    const agile = await user.findOne({
-      where: { job: 'Agile Leader' }
+async function userModel () {
+  const user = createUserModel()
+  // force: true will drop the table if it already exists
+  await user.sync({ force: true })
+  await Promise.all([
+    user.create({
+      username: 'techno01',
+      job: 'Programmer'
+    }),
+    user.create({
+      username: 'techno02',
+      job: 'Head Programmer'
+    }),
+    user.create({
+      username: 'techno03',
+      job: 'Agile Leader'
     })
-    console.log(JSON.stringify(agile, null, 4))
+  ])
 
-    const all = await user.findAll()
-    console.log(JSON.stringify(all, null, 4))
+  const id1 = await user.findByPk(3)
+  console.log(JSON.stringify(id1, null, 4))
 
-    const programmers = await user
-      .findAndCountAll({
-        where: {
-          job: {
-            [Sequelize.Op.like]: '%Programmer'
-          }
-        },
-        limit: 2
-      })
-    console.log(programmers.count)
-    const dataValues = programmers.rows.reduce((aggregate, latest) => {
-      aggregate.push(latest.dataValues)
-      return aggregate
-    }, [])
-    console.log(dataValues)
-
-    const updated = await user.update(
-      { job: 'Scrum Master' },
-      { where: { id: 3 } })
-    console.log(updated)
-    resolve()
+  const agile = await user.findOne({
+    where: { job: 'Agile Leader' }
   })
+  console.log(JSON.stringify(agile, null, 4))
+
+  const all = await user.findAll()
+  console.log(JSON.stringify(all, null, 4))
+
+  const programmers = await user
+    .findAndCountAll({
+      where: {
+        job: {
+          [Sequelize.Op.like]: '%Programmer'
+        }
+      },
+      limit: 2
+    })
+  console.log(programmers.count)
+  const dataValues = programmers.rows.reduce((aggregate, latest) => {
+    aggregate.push(latest.dataValues)
+    return aggregate
+  }, [])
+  console.log(dataValues)
+
+  const updated = await user.update(
+    { job: 'Scrum Master' },
+    { where: { id: 3 } })
+  console.log(updated)
 }
 
 userModel().then(() => {
