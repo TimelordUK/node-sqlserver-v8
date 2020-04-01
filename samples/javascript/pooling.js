cc841bf2377ae7c27aa14ddb7940b8378e9e0ff1
Stephen <sjames8888@gmail.com>
2020-04-01 19:22:21 +0100

    base pool functionality - now need to return a query similar to that from connection

--- samples/javascript/pooling.js
+++ samples/javascript/pooling.js
@@ -3,5 +3,13 @@ const sql = require('msnodesqlv8')
 const pool = new sql.Pool()
 pool.on('open', () => {
   console.log('ready')
+  for (let i = 0; i < 7; ++i) {
+    const sql = 'waitfor delay \'00:00:10\';'
+    pool.query(sql)
+  }
+})
+pool.on('submitted', q => {
+  console.log(`query submitted ${new Date()}`)
+  q.on('done', () => console.log(`query done ${new Date()}`))
 })
 pool.open()
