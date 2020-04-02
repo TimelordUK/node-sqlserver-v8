7c6adfba90f0c7ae3906363837456f3ac6617b0f
Stephen <sjames8888@gmail.com>
2020-04-03 00:15:53 +0100

    start to build a test suite for the pool

---
+++ unit.tests/connection-pool.js
@@ -0,0 +1,89 @@
+
+/* global suite teardown teardown test setup */
+'use strict'
+
+const assert = require('assert')
+const supp = require('../samples/typescript/demo-support')
+
+suite('connection-pool', function () {
+  this.timeout(30 * 1000)
+  const sql = global.native_sql
+
+  let theConnection
+  let connStr
+  let support
+  let helper
+  let procedureHelper
+
+  setup(done => {
+    supp.GlobalConn.init(sql, co => {
+      connStr = global.conn_str || co.conn_str
+      support = co.support
+      procedureHelper = new support.ProcedureHelper(connStr)
+      procedureHelper.setVerbose(false)
+      helper = co.helper
+      helper.setVerbose(false)
+      sql.open(connStr, (err, newConn) => {
+        assert(err === false)
+        theConnection = newConn
+        done()
+      })
+    }, global.conn_str)
+  })
+
+  teardown(done => {
+    theConnection.close(err => {
+      assert.ifError(err)
+      done()
+    })
+  })
+
+  test('submit 4 queries to pool of 4 connections', testDone => {
+    const pool = new sql.Pool({
+      connectionString: 'Driver={ODBC Driver 13 for SQL Server};Server=(localdb)\\node;Database=scratch;Trusted_Connection=yes;'
+    })
+    const iterations = 4
+    pool.on('error', e => {
+      assert.ifError(e)
+    })
+    pool.open()
+
+    const checkin = []
+    const checkout = []
+    pool.on('open', () => {
+      pool.on('status', s => {
+        if (s.op === 'checkout') {
+          checkout.push(s)
+        } else if (s.op === 'checkin') {
+          checkin.push(s)
+        }
+        if (checkin.length === iterations) {
+          assert.strictEqual(iterations, checkout.length)
+          assert.strictEqual(iterations, checkin.length)
+          assert.strictEqual(iterations, done)
+          const elapsed = checkin[checkin.length - 1].time - checkout[0].time
+          assert(elapsed >= 4000 && elapsed <= 5000)
+          pool.close()
+        }
+      })
+    })
+
+    pool.on('close', () => {
+      testDone()
+    })
+
+    let done = 0
+
+    function submit (sql) {
+      const q = pool.query(sql)
+      q.on('submitted', () => {
+        q.on('done', () => ++done)
+      })
+      return q
+    }
+
+    for (let i = 0; i < iterations; ++i) {
+      submit(`waitfor delay '00:00:0${i + 1}';`)
+    }
+  })
+})
