38d58761bf673e79e76af68682a37ffc960e5543
Stephen <sjames8888@gmail.com>
2020-04-02 14:26:44 +0100

    let pool fall back to min connection count if pool is idle

--- samples/javascript/pooling.js
+++ samples/javascript/pooling.js
@@ -6,16 +6,29 @@ pool.on('open', () => {
 })
 
 pool.on('debug', msg => {
-  console.log(`\t\t\t\t\t\t<debug> ${msg}`)
+  console.log(`\t\t\t\t\t\t<pool.debug> ${msg}`)
 })
 
-for (let i = 0; i < 7; ++i) {
-  const sql = 'waitfor delay \'00:00:10\';'
+pool.on('error', e => {
+  console.log(e)
+})
+
+const testSql = 'waitfor delay \'00:00:10\';'
+
+function submit (sql) {
   const q = pool.query(sql)
   q.on('submitted', d => {
-    console.log(`query submitted ${new Date()}`)
-    q.on('done', () => console.log(`query done ${new Date()}`))
+    console.log(`query submitted ${new Date().toLocaleTimeString()}, sql = ${d.query_str}`)
+    q.on('done', () => console.log(`query done ${new Date().toLocaleTimeString()}`))
   })
 }
+
+for (let i = 0; i < 7; ++i) {
+  submit(testSql)
+}
+
+setInterval(() => {
+  submit(testSql)
+}, 60000)
 
 pool.open()
