5ec0b352af12fd5110e38f2398a54d89b61263b4
Stephen <sjames8888@gmail.com>
2020-07-12 18:15:33 +0100

    handle case where repeated info messages are passed back to query such as in back up request where the query could be terminated too early

---
+++ samples/typescript/db-backup.ts
@@ -0,0 +1,25 @@
+import { SqlClient, Error } from "msnodesqlv8"
+const sql: SqlClient = require("msnodesqlv8")
+
+const connectionString = 'Driver={SQL Server Native Client 11.0};Server=(localdb)\\node;Database=AdventureWorks2019;Trusted_Connection=yes;'
+const sqlQuery = `BACKUP DATABASE [AdventureWorks2019] TO  DISK = N'H:\\sql server\\backups\\AdventureWorks2019.bak' WITH  COPY_ONLY, 
+NOFORMAT, INIT,  NAME = N'SampleDb-Full Database Backup', SKIP, NOREWIND, NOUNLOAD,  STATS = 10`;
+
+const query = sql.query(connectionString, sqlQuery, (err, rows, more) => {
+    if (more) return
+    console.error(`cb more ${more}`)
+    console.error(`cb err`)
+    console.log(`cb rows`)
+})
+
+query.on('info', info => {
+    console.log(`info: ${info}`)
+})
+
+query.on('error', info => {
+    console.log(`error: ${info}`)
+})
+
+query.on('done', () => {
+    console.log('done')
+})
