83a99573dd56878ebb82e8489ac5d5fc6d336e46
Stephen James <sjames8888@gmail.com>
2020-07-21 15:00:35 +0100

    bump version to 2 for linux support

--- unit.tests/query.js
+++ unit.tests/query.js
@@ -41,8 +41,8 @@ suite('query', function () {
       async = co.async
       helper = co.helper
       driver = co.driver
-      var myRegexp = /Driver=\{(.*)\}.*$/g;
-      var match = myRegexp.exec(connStr);
+      var myRegexp = /Driver=\{(.*?)\}.*$/g
+      var match = myRegexp.exec(connStr)
       driver = match[1]
       // database = co.database
       helper.setVerbose(false)
