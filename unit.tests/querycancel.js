54b13362c534d85dc7b6801461918de933175cdf
TimelordUK <sjames8888@gmail.com>
2020-08-22 12:18:12 +0100

    query cancel under async model

--- unit.tests/querycancel.js
+++ unit.tests/querycancel.js
@@ -40,6 +40,58 @@ suite('querycancel', function () {
     })
   })
 
+  test('cancel a call to proc that waits for delay of input param.', testDone => {
+    const spName = 'test_spwait_for'
+
+    const def = 'alter PROCEDURE <name>' +
+      '(\n' +
+      '@timeout datetime' +
+      '\n)' +
+      'AS\n' +
+      'BEGIN\n' +
+      'waitfor delay @timeout;' +
+      'END\n'
+
+    const fns = [
+      asyncDone => {
+        procedureHelper.createProcedure(spName, def, () => {
+          asyncDone()
+        })
+      },
+
+      asyncDone => {
+        const pm = theConnection.procedureMgr()
+        pm.setPolling(true)
+        const q = pm.callproc(spName, ['0:0:20'], err => {
+          assert(err)
+          assert(err.message.indexOf('Operation canceled') > 0)
+          asyncDone()
+        })
+        q.on('submitted', () => {
+          q.cancelQuery(err => {
+            assert(!err)
+          })
+        })
+      }
+    ]
+
+    async.series(fns, () => {
+      testDone()
+    })
+  })
+
+  test('cancel single waitfor using notifier - expect Operation canceled', testDone => {
+    const q = theConnection.query(sql.PollingQuery('waitfor delay \'00:00:20\';'), err => {
+      assert(err)
+      assert(err.message.indexOf('Operation canceled') > 0)
+      testDone()
+    })
+
+    q.cancelQuery(err => {
+      assert(!err)
+    })
+  })
+
   test('nested cancel - expect Operation canceled on both', testDone => {
     const q1 = theConnection.query(sql.PollingQuery('waitfor delay \'00:00:50\';'), err => {
       assert(err.message.indexOf('Operation canceled') > 0)
@@ -78,60 +130,6 @@ suite('querycancel', function () {
     })
   })
 
-  test('cancel a prepared call that waits', testDone => {
-    const s = 'waitfor delay ?;'
-    let prepared
-
-    const fns = [
-      asyncDone => {
-        theConnection.prepare(sql.PollingQuery(s), (err, pq) => {
-          assert(!err)
-          prepared = pq
-          asyncDone()
-        })
-      },
-
-      asyncDone => {
-        const q = prepared.preparedQuery(['00:00:20'], err => {
-          assert(err)
-          assert(err.message.indexOf('Operation canceled') > 0)
-          asyncDone()
-        })
-
-        q.on('submitted', () => {
-          q.cancelQuery(err => {
-            assert(!err)
-          })
-        })
-      }
-    ]
-
-    async.series(fns, () => {
-      testDone()
-    })
-  })
-
-  test('pause a large query and cancel check done', testDone => {
-    let rows = 0
-    const q = theConnection.query('select top 3000 * from syscolumns')
-    q.on('error', (e) => {
-      assert.ifError(e)
-    })
-    q.on('row', () => {
-      ++rows
-      if (rows % 100 === 0) {
-        q.pauseQuery()
-        setTimeout(() => {
-          q.cancelQuery(() => {
-          })
-        }, 50)
-      }
-    })
-    q.on('done', () => {
-      testDone()
-    })
-  })
-
   test('cancel single query from notifier using tmp connection - expect Operation canceled', testDone => {
     const q = sql.query(connStr, sql.PollingQuery('waitfor delay \'00:00:59\';'), err => {
       assert(err)
@@ -166,18 +164,6 @@ suite('querycancel', function () {
     theConnection.cancelQuery(q, err => {
       assert(err)
       assert(err.message.indexOf('only supported') > 0)
-    })
-  })
-
-  test('cancel single waitfor using notifier - expect Operation canceled', testDone => {
-    const q = theConnection.query(sql.PollingQuery('waitfor delay \'00:00:20\';'), err => {
-      assert(err)
-      assert(err.message.indexOf('Operation canceled') > 0)
-      testDone()
-    })
-
-    q.cancelQuery(err => {
-      assert(!err)
     })
   })
 
@@ -226,46 +212,6 @@ suite('querycancel', function () {
     })
   })
 
-  test('cancel a call to proc that waits for delay of input param.', testDone => {
-    const spName = 'test_spwait_for'
-
-    const def = 'alter PROCEDURE <name>' +
-      '(\n' +
-      '@timeout datetime' +
-      '\n)' +
-      'AS\n' +
-      'BEGIN\n' +
-      'waitfor delay @timeout;' +
-      'END\n'
-
-    const fns = [
-      asyncDone => {
-        procedureHelper.createProcedure(spName, def, () => {
-          asyncDone()
-        })
-      },
-
-      asyncDone => {
-        const pm = theConnection.procedureMgr()
-        pm.setPolling(true)
-        const q = pm.callproc(spName, ['0:0:20'], err => {
-          assert(err)
-          assert(err.message.indexOf('Operation canceled') > 0)
-          asyncDone()
-        })
-        q.on('submitted', () => {
-          q.cancelQuery(err => {
-            assert(!err)
-          })
-        })
-      }
-    ]
-
-    async.series(fns, () => {
-      testDone()
-    })
-  })
-
   test('2 x cancel - expect Operation canceled on both', testDone => {
     let hits = 0
 
@@ -292,6 +238,60 @@ suite('querycancel', function () {
 
     theConnection.cancelQuery(q2, err => {
       assert(!err)
+    })
+  })
+
+  test('pause a large query and cancel check done', testDone => {
+    let rows = 0
+    const q = theConnection.query('select top 3000 * from syscolumns')
+    q.on('error', (e) => {
+      assert.ifError(e)
+    })
+    q.on('row', () => {
+      ++rows
+      if (rows % 100 === 0) {
+        q.pauseQuery()
+        setTimeout(() => {
+          q.cancelQuery(() => {
+          })
+        }, 50)
+      }
+    })
+    q.on('done', () => {
+      testDone()
+    })
+  })
+
+  test('cancel a prepared call that waits', testDone => {
+    const s = 'waitfor delay ?;'
+    let prepared
+
+    const fns = [
+      asyncDone => {
+        theConnection.prepare(sql.PollingQuery(s), (err, pq) => {
+          assert(!err)
+          prepared = pq
+          asyncDone()
+        })
+      },
+
+      asyncDone => {
+        const q = prepared.preparedQuery(['00:00:20'], err => {
+          assert(err)
+          assert(err.message.indexOf('Operation canceled') > 0)
+          asyncDone()
+        })
+
+        q.on('submitted', () => {
+          q.cancelQuery(err => {
+            assert(!err)
+          })
+        })
+      }
+    ]
+
+    async.series(fns, () => {
+      testDone()
     })
   })
 })
