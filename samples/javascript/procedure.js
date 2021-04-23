871b6fc1d604faf8f96da5ca2398f7262fe7f442
Stephen James <sjames8888@gmail.com>
2021-04-24 00:02:27 +0100

    enhance proc demo

--- samples/javascript/procedure.js
+++ samples/javascript/procedure.js
@@ -29,13 +29,38 @@ const longDef = `create PROCEDURE ${longspName} (
     select top 100 * from syscolumns;
   END
   `
-
+const helloSpName = 'hello_sp'
+const helloSpDef = `create PROCEDURE ${helloSpName} AS
+    BEGIN
+      print 'hello';
+      select top 100 * from sysobjects;
+      print 'world';
+      select top 100 * from syscolumns;
+    END
+    `
 async function main (invocations) {
+  await asConnectionMessages(10000, 1)
   await asConnectionTimeout(10000, 2)
   await asConnectionTimeout(200, 1)
   await asConnectionTimeout(200, 1)
   await asConnection(invocations)
   await asPool(invocations)
+}
+
+async function asConnectionMessages (procTimeout, invocations) {
+  const connectionString = getConnection()
+  const promisedOpen = util.promisify(sql.open)
+  const con = await promisedOpen(connectionString)
+  const helper = new ProcedureHelper(con, helloSpName, helloSpDef)
+  await helper.create()
+  for (let i = 0; i < invocations; ++i) {
+    try {
+      const res = await helper.callProcPromise([], procTimeout)
+      console.log(`${helloSpName}: testTimeout [${i} - ${procTimeout}] done - sets returned ${res.results.length} [${res.results.map(r => r.length).join(', ')}] [${res.info.join(', ')}]`)
+    } catch (e) {
+      console.log(e.message)
+    }
+  }
 }
 
 async function asConnectionTimeout (procTimeout, invocations) {
@@ -55,7 +80,7 @@ async function testTimeout (connectionProxy, invocations, procTimeout) {
   for (let i = 0; i < invocations; ++i) {
     try {
       const res = await helper.callProcPromise(p, procTimeout)
-      console.log(`testTimeout [${i} - ${procTimeout}] done - sets returned ${res.results.length} [${res.results.map(r => r.length).join(', ')}]`)
+      console.log(`${longspName}: testTimeout [${i} - ${procTimeout}] done - sets returned ${res.results.length} [${res.results.map(r => r.length).join(', ')}]`)
     } catch (e) {
       console.log(`rejection: ${e.message} - test connection still good.`)
       // test can still use the connection
@@ -75,7 +100,7 @@ async function asConnection (invocations) {
     const promisedOpen = util.promisify(sql.open)
     const con = await promisedOpen(connectionString)
     const res = await test(con, invocations)
-    console.log(`asConnection ${invocations} invocations, elapsed = ${res.elapsed}, res length=${res.results.length}`)
+    console.log(`${spName}: asConnection ${invocations} invocations, elapsed = ${res.elapsed}, res length=${res.results.length}`)
     const promisedClose = util.promisify(con.close)
     await promisedClose()
   } catch (err) {
@@ -98,7 +123,7 @@ async function asPool (invocations) {
     const options = await promisedOpen()
     console.log(`pool opened : ${JSON.stringify(options, null, 4)}`)
     const res = await test(pool, invocations)
-    console.log(`asPool [${size}] ${invocations} invocations, elapsed = ${res.elapsed}, res length=${res.results.length}`)
+    console.log(`${spName}: asPool [${size}] ${invocations} invocations, elapsed = ${res.elapsed}, res length=${res.results.length}`)
     const promisedClose = util.promisify(pool.close)
     await promisedClose()
   } catch (err) {
@@ -141,57 +166,86 @@ class ProcedureHelper {
   constructor (connectionProxy, spName, def) {
     const dropProcedureSql = `IF EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND OBJECT_ID = OBJECT_ID('${spName}'))
     begin drop PROCEDURE ${spName} end `
-    const promisedQuery = util.promisify(connectionProxy.query)
 
-    async function create () {
-      await promisedQuery(dropProcedureSql)
-      await promisedQuery(def)
-    }
+    this.procName = spName
+    this.defition = def
+    this.dropProcedureSql = dropProcedureSql
+    this.connectionProxy = connectionProxy
+  }
 
-    function callProcPromise (o, timeoutMs) {
-      return new Promise((resolve, reject) => {
-        let handle = null
-        const ret = {
-          results: [],
-          output: null,
-          info: null
+  async create () {
+    const promisedQuery = util.promisify(this.connectionProxy.query)
+    await promisedQuery(this.dropProcedureSql)
+    await promisedQuery(this.defition)
+  }
+
+  callProcPromise (o, timeoutMs) {
+    return new Promise((resolve, reject) => {
+      let handle = null
+      let row = {}
+      const ret = {
+        meta: [],
+        results: [],
+        output: null,
+        info: null
+      }
+      const q = this.connectionProxy.callproc(this.procName, o)
+      if (timeoutMs) {
+        handle = setTimeout(() => {
+          try {
+            q.pauseQuery()
+            q.cancelQuery((e) => {
+              reject(e || new Error(`query cancelled timeout ${timeoutMs}`))
+            })
+          } catch (e) {
+            reject(e)
+          }
+        }, timeoutMs)
+      }
+
+      q.on('output', o => {
+        ret.output = o
+      })
+
+      q.on('error', e => {
+        reject(e)
+      })
+
+      q.on('info', m => {
+        if (!ret.info) {
+          ret.info = []
         }
-        const q = connectionProxy.callproc(spName, o, (err, results, output, more) => {
-          if (err) {
-            reject(err)
-          } else {
-            ret.results.push(results)
-            if (!more) {
-              if (handle) {
-                clearTimeout(handle)
-              }
-              ret.output = output
-              resolve(ret)
-            }
-          }
-        })
-        q.on('info', m => {
-          if (!ret.info) {
-            ret.info = []
-          }
-          ret.info.push(m)
-        })
-        if (timeoutMs) {
-          handle = setTimeout(() => {
-            try {
-              q.pauseQuery()
-              q.cancelQuery((e) => {
-                reject(e || new Error(`query cancelled timeout ${timeoutMs}`))
-              })
-            } catch (e) {
-              reject(e)
-            }
-          }, timeoutMs)
+        ret.info.push(m.message.substr(m.message.lastIndexOf(']') + 1))
+      })
+
+      q.on('meta', meta => {
+        ret.meta.push(meta)
+        ret.results.push([])
+      })
+
+      q.on('row', e => {
+        row = {}
+      })
+
+      q.on('done', () => {
+        if (handle) {
+          clearTimeout(handle)
         }
       })
-    }
 
-    this.callProcPromise = callProcPromise
-    this.create = create
+      q.on('free', () => {
+        resolve(ret)
+      })
+
+      q.on('column', (c, v) => {
+        const resultId = ret.meta.length - 1
+        const meta = ret.meta[resultId]
+        const results = ret.results[resultId]
+        row[meta[c].name] = v
+        if (c === meta.length - 1) {
+          results.push(row)
+        }
+      })
+    })
   }
 }
