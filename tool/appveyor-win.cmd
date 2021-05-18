be5de1f9a38c6218686f52b02723b93ca7a07029
Stephen <sjames8888@gmail.com>
2021-05-18 21:05:28 +0100

    appv

--- tool/appveyor-win.cmd
+++ tool/appveyor-win.cmd
@@ -5,13 +5,6 @@ call node unit.tests\cmd-test.js -a 2017 -t benchmark --repeats=5 --delay=4500 2
 call node unit.tests\cmd-test.js -a 2017 -t benchmark --table=syscolumns --delay=5000 --repeats=5 2>&1
 call node runtest -a 2017 -t json -t sproc.js -t connection-pool -t pause -t concurrent -t multiple-errors -t geography -t tvp -t warnings.js -t compoundqueries.js -t querycancel.js -t txn.js -t datatypes.js -t params.js -t query.js -t querytimeout.js -t connect.js -t bulk.js -t prepared.js -t userbind.js -t dates.js 2>&1
 call net stop MSSQL$SQL2017
-SET MSSQL_VERSION=2012
-call powershell tool\appveyor.ps1 SQL2012SP1
-call node --version
-call node unit.tests\cmd-test.js -a 2012SP1 -t benchmark --repeats=5 --delay=4500 2>&1
-call node unit.tests\cmd-test.js -a 2012SP1 -t benchmark --table=syscolumns --delay=5000 --repeats=5 2>&1
-call node runtest -a 2012SP1 -t json -t sproc.js -t connection-pool -t pause -t concurrent -t multiple-errors -t geography -t tvp -t warnings.js -t compoundqueries.js -t querycancel.js -t txn.js -t datatypes.js -t params.js -t query.js -t querytimeout.js -t connect.js -t bulk.js -t prepared.js -t userbind.js -t dates.js 2>&1
-call net stop MSSQL$SQL2012SP1
 SET MSSQL_VERSION=2014
 call powershell tool\appveyor.ps1 SQL2014
 call node --version
