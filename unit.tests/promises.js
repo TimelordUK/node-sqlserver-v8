fb2c571788724f0924d890c47c2e633bbdb60c8c
Stephen James <sjames8888@gmail.com>
2021-09-08 20:01:28 +0100

    promises

--- unit.tests/promises.js
+++ unit.tests/promises.js
@@ -73,14 +73,122 @@ suite('promises', function () {
     async insertSelect (table) {
       const parsedJSON = this.createEmployees(200)
       table.setUseBcp(true)
-      const d = new Date()
       await table.promises.insert(parsedJSON)
-      console.log(`ms = ${new Date() - d}`)
       const keys = helper.extractKey(parsedJSON, 'BusinessEntityID')
       const results = await table.promises.select(keys)
       assert.deepStrictEqual(results, parsedJSON, 'results didn\'t match')
     }
   }
+
+  class BulkTableTest {
+    constructor (c, def) {
+      function where (list, primitive) {
+        return list.reduce((agg, latest) => {
+          if (primitive(latest)) {
+            agg.push(latest)
+          }
+          return agg
+        }, [])
+      }
+      const tableName = def.tableName
+      const columns = def.columns.map(e => `${e.name} ${e.type}`).join(', ')
+      const insertColumnNames = where(def.columns, c => {
+        const res = !c.type.includes('identity')
+        return res
+      }).map(e => `${e.name}`).join(', ')
+      const columnNames = def.columns.map(e => `${e.name}`).join(', ')
+      const dropTableSql = `IF OBJECT_ID('${tableName}', 'U') IS NOT NULL DROP TABLE ${tableName};`
+      const createTableSql = `CREATE TABLE ${tableName} (${columns})`
+      const clusteredSql = `CREATE CLUSTERED INDEX IX_${tableName} ON ${tableName}(id)`
+      const insertSql = `INSERT INTO ${tableName} (${insertColumnNames}) VALUES `
+      const selectSql = `SELECT ${columnNames} FROM ${tableName}`
+      const trucateSql = `TRUNCATE TABLE ${tableName}`
+      const paramsSql = `(${def.columns.map(_ => '?').join(', ')})`
+
+      this.definition = def
+      this.theConnection = c
+      this.dropTableSql = dropTableSql
+      this.createTableSql = createTableSql
+      this.clusteredSql = clusteredSql
+      this.selectSql = selectSql
+      this.insertSql = insertSql
+      this.truncateSql = trucateSql
+      this.tableName = def.tableName
+      this.paramsSql = paramsSql
+      this.insertParamsSql = `${insertSql} ${paramsSql}`
+    }
+
+    async create () {
+      const promises = theConnection.promises
+      await promises.query(this.dropTableSql)
+      await promises.query(this.createTableSql)
+      const table = await promises.getTable(this.tableName)
+      return table
+    }
+  }
+
+  test('promises for table insert select rows', testDone => {
+    const bulkTableDef = {
+      tableName: 'test_table_bulk',
+      columns: [
+        {
+          name: 'id',
+          type: 'INT PRIMARY KEY'
+        },
+        {
+          name: 'd1',
+          type: 'datetime'
+        },
+        {
+          name: 's1',
+          type: 'VARCHAR (255) NOT NULL'
+        },
+        {
+          name: 's2',
+          type: 'VARCHAR (100) NOT NULL'
+        },
+        {
+          name: 's3',
+          type: 'VARCHAR (50) NOT NULL'
+        },
+        {
+          name: 's4',
+          type: 'VARCHAR (50) NOT NULL'
+        }
+      ]
+    }
+    async function runner () {
+      const helper = new BulkTableTest(theConnection, bulkTableDef)
+      const testDate = new Date('Mon Apr 26 2021 22:05:38 GMT-0500 (Central Daylight Time)')
+      const expected = []
+      const rows = 3000
+      for (let i = 0; i < rows; ++i) {
+        expected.push({
+          id: i,
+          d1: testDate,
+          s1: `${i}`,
+          s2: `testing${i + 1}2Data`,
+          s3: `testing${i + 2}2Data`,
+          s4: `testing${i + 3}2Data`
+        })
+      }
+      theConnection.setUseUTC(false)
+      const table = await helper.create()
+      try {
+        await table.promises.insert(expected)
+        const res = await table.promises.select(expected)
+        res.forEach(a => {
+          delete a.d1.nanosecondsDelta
+        })
+        assert.deepStrictEqual(res, expected)
+      } catch (e) {
+        return e
+      }
+    }
+    runner().then((e) => {
+      testDone(e)
+    })
+  })
 
   test('using promises to open, query, close pool', testDone => {
     async function exec () {
