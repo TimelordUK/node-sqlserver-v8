691b232bc1d7416bca6815d365d556d134373b3c
Stephen James <sjames8888@gmail.com>
2021-09-09 23:11:08 +0100

    promises demo

--- samples/typescript/promises.ts
+++ samples/typescript/promises.ts
@@ -1,4 +1,4 @@
-import {Error, PoolOptions, Query, SqlClient, QueryDescription, Pool, PoolStatusRecord, Connection} from 'msnodesqlv8';
+import {Error, PoolOptions, Query, SqlClient, QueryDescription, Pool, PoolStatusRecord, Connection, QueryAggregatorResults, ConnectionPromises} from 'msnodesqlv8';
 
 // require the module so it can be used in your node JS code.
 export const sql : SqlClient = require('msnodesqlv8');
@@ -13,7 +13,7 @@ async function openSelectClose() {
     try {
         const connStr: string = getConnection()
         const conn: Connection = await sql.promises.open(connStr)
-        const res = await conn.promises.query('select @@SPID as spid')
+        const res: QueryAggregatorResults = await conn.promises.query('select @@SPID as spid')
         console.log(JSON.stringify(res, null, 4))
         await conn.promises.close()
     } catch (e: any) {
@@ -24,7 +24,7 @@ async function openSelectClose() {
 async function ashoc() {
     try {
         const connStr: string = getConnection()
-        const res = await sql.promises.query(connStr, 'select @@SPID as spid')
+        const res: QueryAggregatorResults = await sql.promises.query(connStr, 'select @@SPID as spid')
         console.log(`ashoc spid ${res.first[0].spid}`)
     } catch (e: any) {
         console.log(e)
@@ -40,7 +40,7 @@ async function pool() {
     })
     await pool.promises.open()
     const all = Array(size * 2).fill(0).map((_, i) => pool.promises.query(`select ${i} as i, @@SPID as spid`))
-    const promised = await Promise.all(all)
+    const promised: QueryAggregatorResults[] = await Promise.all(all)
     const res = promised.map(r => r.first[0].spid)
     await pool.promises.close()
     console.log(`pool spids ${res.join(', ')}`)
@@ -58,13 +58,14 @@ async function proc() {
     begin drop PROCEDURE ${spName} end `
     const connStr: string = getConnection()
     const conn: Connection = await sql.promises.open(connStr)
-    await conn.promises.query(dropProcedureSql)
-    await conn.promises.query(def)
+    const promises: ConnectionPromises = conn.promises
+    await promises.query(dropProcedureSql)
+    await promises.query(def)
     const msg = 'hello world'
-    const res = await conn.promises.callProc(spName, {
+    const res: QueryAggregatorResults = await promises.callProc(spName, {
         param: msg
       })
-    await conn.promises.close()
+    await promises.close()
 
     console.log(`proc returns ${res.returns} from param '${msg}''`)
 }
