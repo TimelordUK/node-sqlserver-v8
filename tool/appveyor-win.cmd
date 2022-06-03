aa187d16dae7ef4af598b6956ff3575e78a0e8a4
Stephen James <sjames8888@gmail.com>
2022-06-03 10:39:50 +0100

    testing

--- tool/appveyor-win.cmd
+++ tool/appveyor-win.cmd
@@ -3,7 +3,7 @@ call powershell tool\appveyor.ps1 SQL2017
 call node --version
 call .\node_modules\.bin\env-cmd -e appv-2017 node test\env\cmd-test -t benchmark --repeats=5 --delay=4500 2>&1 
 call .\node_modules\.bin\env-cmd -e appv-2017 node test\env\cmd-test -t benchmark --table=syscolumns --repeats=5 --delay=5000 2>&1
-call .\node_modules\.bin\env-cmd -e appv-2017 node_modules\.bin\mocha --ignore test\bcp.js 2>&1
+call .\node_modules\.bin\env-cmd -e appv-2017 node_modules\.bin\mocha --exclude **\bcp.js 2>&1
 call net stop MSSQL$SQL2017
 
 SET MSSQL_VERSION=2014
@@ -11,7 +11,7 @@ call powershell tool\appveyor.ps1 SQL2014
 call node --version
 call .\node_modules\.bin\env-cmd -e appv-2014 node test\env\cmd-test -t benchmark --repeats=5 --delay=4500 2>&1 
 call .\node_modules\.bin\env-cmd -e appv-2014 node test\env\cmd-test -t benchmark --table=syscolumns --repeats=5 --delay=5000 2>&1
-call .\node_modules\.bin\env-cmd -e appv-2014 node_modules\.bin\mocha --ignore test\bcp.js 2>&1
+call .\node_modules\.bin\env-cmd -e appv-2014 node_modules\.bin\mocha --exclude **\bcp.js 2>&1
 call net stop MSSQL$SQL2014
 
 SET MSSQL_VERSION=2016
@@ -19,5 +19,5 @@ call powershell tool\appveyor.ps1 SQL2016
 call node --version
 call .\node_modules\.bin\env-cmd -e appv-2016 node test\env\cmd-test -t benchmark --repeats=5 --delay=4500 2>&1 
 call .\node_modules\.bin\env-cmd -e appv-2016 node test\env\cmd-test -t benchmark --table=syscolumns --repeats=5 --delay=5000 2>&1
-call .\node_modules\.bin\env-cmd -e appv-2016 node_modules\.bin\mocha --ignore test\bcp.js 2>&1
+call .\node_modules\.bin\env-cmd -e appv-2016 node_modules\.bin\mocha --exclude **\bcp.js 2>&1
 call net stop MSSQL$SQL2016
