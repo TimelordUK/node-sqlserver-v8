c72737a0eaaa79e7b33414d64205e5e5961219ab
Stephen James <sjames8888@gmail.com>
2022-07-15 19:07:00 +0100

    testing

--- tool/appveyor-win.cmd
+++ tool/appveyor-win.cmd
@@ -1,3 +1,13 @@
+SET MSSQL_VERSION=2019
+
+call powershell tool\appveyor.ps1 SQL2019
+call node --version
+call .\node_modules\.bin\env-cmd -e appv-2019 npm run bench-comments 
+call .\node_modules\.bin\env-cmd -e appv-2019 npm run bench-columns
+call .\node_modules\.bin\env-cmd -e appv-v17-2019 node_modules\.bin\mocha 2>&1
+call .\node_modules\.bin\env-cmd -e appv-2019 node_modules\.bin\mocha --exclude **/bcp.js 2>&1
+call net stop MSSQL$SQL2019
+
 SET MSSQL_VERSION=2017
 
 call powershell tool\appveyor.ps1 SQL2017
@@ -7,19 +17,3 @@ call .\node_modules\.bin\env-cmd -e appv-2017 npm run bench-columns
 call .\node_modules\.bin\env-cmd -e appv-v17-2017 node_modules\.bin\mocha 2>&1
 call .\node_modules\.bin\env-cmd -e appv-2017 node_modules\.bin\mocha --exclude **/bcp.js 2>&1
 call net stop MSSQL$SQL2017
-
-SET MSSQL_VERSION=2014
-call powershell tool\appveyor.ps1 SQL2014
-call node --version
-call .\node_modules\.bin\env-cmd -e appv-2014 npm run bench-comments
-call .\node_modules\.bin\env-cmd -e appv-2014 npm run bench-columns
-call .\node_modules\.bin\env-cmd -e appv-2014 node_modules\.bin\mocha --exclude **/bcp.js --exclude **/json.js 2>&1
-call net stop MSSQL$SQL2014
-
-SET MSSQL_VERSION=2016
-call powershell tool\appveyor.ps1 SQL2016
-call node --version
-call .\node_modules\.bin\env-cmd -e appv-2016 npm run bench-comments 
-call .\node_modules\.bin\env-cmd -e appv-2016 npm run bench-columns
-call .\node_modules\.bin\env-cmd -e appv-2016 node_modules\.bin\mocha --exclude **/bcp.js --exclude **/json.js 2>&1
-call net stop MSSQL$SQL2016
