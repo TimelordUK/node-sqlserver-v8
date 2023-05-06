f8a869a492ef7d91f7f850313c8acb21a924e723
stephen james <sjames8888@gmail.com>
2023-05-06 13:50:34 +0100

    appveyor

--- tool/install_openssl.sh
+++ tool/install_openssl.sh
@@ -1,4 +1,5 @@
-$clone_folder=$HOME/projects
+$clone_folder=$HOME/projects/node_modules/msnodesqlv8
+echo "node version $nodejs_version"
  if [ "$nodejs_version" -ge "18" ]; then
     cd $clone_folder
     tool/openssl.sh
