36c5fab5116e1a404d6118e42063e88fc6054dda
stephen james <sjames8888@gmail.com>
2023-05-06 13:35:52 +0100

    fixes for opensll script

--- tool/openssl.sh
+++ tool/openssl.sh
@@ -3,15 +3,14 @@ then
     echo "Ubuntu $(lsb_release -rs) is not currently supported.";
     exit;
 fi
-
-cd /home/appveyor/projects
+mkdir -p $HOME/projects
+cd $HOME/projects
 git clone https://github.com/openssl/openssl.git
 cd openssl
-./config --prefix=/usr/local/ssl --openssldir=/usr/local/ssl shared zlib
+./config --prefix=/usr/local/ssl --openssldir=/usr/local/ssl shared
 make
 # make test
-sudo su
 sudo make install
 cd /etc/ld.so.conf.d/
-echo "/usr/local/ssl/lib64/" > openssl-3.2.0.1s.conf
+echo '/usr/local/ssl/lib64/' | sudo tee openssl-3.2.0.1s.conf
 sudo ldconfig -v
