0a5725d9e9f0bf6a7b19f037e8feb879aa6cedab
TimelordUK <sjames8888@gmail.com>
2023-05-22 16:09:25 +0100

    potential fix for unicode col name strings avoiding corruption on 16 bit char sets

--- src/ResultSet.h
+++ src/ResultSet.h
@@ -33,7 +33,7 @@ namespace mssql
 		typedef vector<shared_ptr<Column>> t_row;
         struct ColumnDefinition
         {
-            string name;
+            wstring name;
             SQLULEN columnSize;
             SQLSMALLINT dataType;
             string dataTypeName;
