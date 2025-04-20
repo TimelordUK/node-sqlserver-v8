d73bc25ca7845770513a60022d11382761734538
TimelordUK <sjames8888@gmail.com>
2025-04-20 22:00:45 +0100

    add promise based api on the close and open methods.

---
+++ test/napi/promise.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const chai = require('chai')
+const assert = chai.assert
+const sql = require('msnodesqlv8')
+sql.setLogLevel(4); // Debug level
+sql.enableConsoleLogging(true);
+const connectionConfig = require('../common/connection-config')
+
+describe('promise', function () {
+    it('use a promise to open a connection', async () => {
+        const cs = connectionConfig.getConnectionString()
+        const connection = new sql.Connection()
+        const c = await connection.promises.open(cs)
+        assert(c !== null)
+        await  connection.promises.close()
+    })
+})
