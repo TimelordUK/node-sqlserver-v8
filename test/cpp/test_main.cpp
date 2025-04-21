a53568dd696de73e548a4434f899c7b42068ad81
TimelordUK <sjames8888@gmail.com>
2025-04-21 17:32:39 +0100

    remove fules

--- test/cpp/test_main.cpp
+++ test/cpp/test_main.cpp
@@ -1,6 +1,9 @@
-// #include <gtest/gtest.h>
+#include <gtest/gtest.h>
 
-// int main(int argc, char** argv) {
-//   ::testing::InitGoogleTest(&argc, argv);
-//   return RUN_ALL_TESTS();
-// }
+// Demonstrate some basic assertions.
+TEST(HelloTest, BasicAssertions) {
+  // Expect two strings not to be equal.
+  EXPECT_STRNE("hello", "world");
+  // Expect equality.
+  EXPECT_EQ(7 * 6, 42);
+}
