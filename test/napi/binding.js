f36a82b020632c90b90f48d818c13cad6a11e3a6
TimelordUK <sjames8888@gmail.com>
2025-04-27 13:43:58 +0100

    add a sql parameter binder so we can move more work of param discovery into js

---
+++ test/napi/binding.js
@@ -0,0 +1,22 @@
+// test/napi/open.js
+'use strict'
+
+const chai = require('chai')
+const assert = chai.assert
+
+const sql = require('msnodesqlv8')
+const { SqlParameter } = sql
+sql.setLogLevel(4) // Debug level
+sql.enableConsoleLogging(true)
+
+describe('binding sql parameters', function () {
+  this.beforeEach(async () => {
+  })
+
+  this.afterEach(async function () {
+  })
+
+  it('bind a simple scalar string', async () => {
+    const stringParam = SqlParameter.fromValue('Hello World', {})()
+  })
+})
