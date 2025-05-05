d15cc5bc18ae527920884bea7552ebd95aeb8e65
TimelordUK <sjames8888@gmail.com>
2025-05-05 16:14:19 +0100

    tests

--- test/napi/bind-table.test.ts
+++ test/napi/bind-table.test.ts
@@ -1,43 +1,40 @@
-import {expect} from 'chai';
-import {Connection} from 'msnodesqlv8/src';
-import * as nativeModule from 'msnodesqlv8';
-import {TestConnectionFactory} from 'msnodesqlv8/test/common/test-connection-factory'
-import {QueryBuilder} from 'msnodesqlv8/src/query-builder'
-import {Logger, LogLevel} from '../../lib/logger'
+import { expect } from 'chai'
+import { Connection } from 'msnodesqlv8/src'
+import * as nativeModule from 'msnodesqlv8'
+import { TestConnectionFactory } from 'msnodesqlv8/test/common/test-connection-factory'
+import { QueryBuilder } from 'msnodesqlv8/src/query-builder'
+import { Logger, LogLevel } from '../../lib/logger'
 
 // Set logging options
-nativeModule.setLogLevel(5); // Debug level
+nativeModule.setLogLevel(5) // Debug level
 nativeModule.enableConsoleLogging(true)
 Logger.getInstance().setMinLevel(LogLevel.DEBUG)
 
-
-describe('bind', function() {
+describe('bind', function () {
   this.timeout(0)
-  let connection: Connection | null = null;
+  let connection: Connection | null = null
   const factory = new TestConnectionFactory()
 
-  beforeEach(async function() {
+  beforeEach(async function () {
     // Create connection using the default connection string
     connection = await factory.createTestConnection()
   })
 
-  afterEach(async function() {
+  afterEach(async function () {
     if (connection) {
       await connection.close()
     }
   })
 
-  it('obtain the schema for a given table', async function() {
+  it('obtain the schema for a given table', async function () {
     const query = QueryBuilder.buildQuery({
       catalog: 'node',
       schema: 'dbo',
-      table: 'Employee',
+      table: 'Employee'
     })
     const sql = query.sql
-    expect(connection).not.to.be.null
     if (connection === null) return
     const res = await connection.query(sql)
     console.log(res)
-
   })
 })
