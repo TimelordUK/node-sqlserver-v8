1aaa1ad4872b415eff88a9ec91089c0b9688c65c
TimelordUK <sjames8888@gmail.com>
2025-05-05 09:40:13 +0100

    add class logger for typescript logging

---
+++ src/async-queue.ts
@@ -0,0 +1,115 @@
+import { ClassLogger } from './class-logger' // Corrected import
+
+/**
+ * Task to be executed in the connection queue
+ */
+export interface QueueTask {
+  execute: (done: () => void) => void
+  fail: (err: Error) => void
+  promise?: Promise<any>
+}
+
+export class AsyncQueue {
+  private readonly _queue: QueueTask[] = []
+  private _busy = false
+  private _closed = false
+  private readonly _logger: ClassLogger
+
+  constructor () {
+    this._logger = new ClassLogger('AsyncQueue')
+    this._logger.debug('AsyncQueue initialized')
+  }
+
+  stop () {
+    this._closed = true
+  }
+
+  size (): number {
+    return this._queue.length
+  }
+
+  /**
+     * Allows statements to enqueue tasks on this connection
+     * This maintains the single-threaded queue while moving logic to Statement
+     */
+  async enqueueTask<T>(task: QueueTask): Promise<T> {
+    return this._enqueue<T>(task)
+  }
+
+  /**
+     * Execute the next task in the queue if not busy
+     */
+  private _executeNext (): void {
+    if (this._queue.length === 0 || this._busy || this._closed) {
+      return
+    }
+
+    this._busy = true
+    const task = this._queue[0]
+
+    // Update logger with current queue size
+    this._logger.setAdditionalContext({ queueSize: this._queue.length })
+    this._logger.debug('Executing next task', { functionName: '_executeNext' })
+
+    try {
+      task.execute(() => {
+        this._queue.shift() // Remove the completed task
+        this._busy = false
+
+        // Update logger with new queue size
+        this._logger.setAdditionalContext({ queueSize: this._queue.length })
+        this._logger.debug('Task completed', { functionName: '_executeNext' })
+
+        this._executeNext() // Check for more tasks
+      })
+    } catch (err: any) {
+      // Handle unexpected errors
+      this._logger.error('Task execution failed', {
+        functionName: '_executeNext',
+        error: err.message
+      })
+      task.fail(err)
+      this._queue.shift()
+      this._busy = false
+      this._executeNext()
+    }
+  }
+
+  /**
+     * Add a task to the execution queue
+     */
+
+  private async _enqueue<T>(task: QueueTask): Promise<T> {
+    if (this._closed) {
+      const err = new Error('Connection is closed')
+      task.fail(err)
+      return Promise.reject(err)
+    }
+
+    // Create a promise if not already defined
+    if (!task.promise) {
+      task.promise = new Promise<T>((resolve, reject) => {
+        const originalExecute = task.execute
+        const originalFail = task.fail
+
+        // Override execute to resolve the promise
+        task.execute = (done) => {
+          originalExecute(() => {
+            resolve(undefined as any)
+            done()
+          })
+        }
+
+        // Override fail to reject the promise
+        task.fail = (err) => {
+          originalFail(err)
+          reject(err)
+        }
+      })
+    }
+
+    this._queue.push(task)
+    this._executeNext()
+    return task.promise as Promise<T>
+  }
+}
--- src/connection.ts
+++ src/connection.ts
@@ -2,106 +2,13 @@
 import { NativeConnection } from './native-module'
 import { EventEmitter } from 'events'
 import { Logger, LogLevel } from './logger' // Corrected import
-import {StatementState} from "./statement";
+import { StatementState } from './statement'
+import { AsyncQueue, QueueTask } from './async-queue'
+import { ClassLogger } from './class-logger'
 
 /**
  * Wrapper for the native Connection class that enforces operation queuing
  */
-
-/**
- * Task to be executed in the connection queue
- */
-export interface QueueTask {
-  execute: (done: () => void) => void
-  fail: (err: Error) => void
-  promise?: Promise<any>
-}
-
-export class AsyncQueue {
-  private readonly _queue: QueueTask[] = []
-  private _busy = false
-  private _closed = false
-
-  stop () {
-    this._closed = true
-  }
-
-  size (): number {
-    return this._queue.length
-  }
-
-  /**
-   * Allows statements to enqueue tasks on this connection
-   * This maintains the single-threaded queue while moving logic to Statement
-   */
-  async enqueueTask<T>(task: QueueTask): Promise<T> {
-    return this._enqueue<T>(task)
-  }
-
-  /**
-   * Execute the next task in the queue if not busy
-   */
-  private _executeNext (): void {
-    if (this._queue.length === 0 || this._busy || this._closed) {
-      return
-    }
-
-    this._busy = true
-    const task = this._queue[0]
-
-    try {
-      task.execute(() => {
-        this._queue.shift() // Remove the completed task
-        this._busy = false
-        this._executeNext() // Check for more tasks
-      })
-    } catch (err: any) {
-      // Handle unexpected errors
-      task.fail(err)
-      this._queue.shift()
-      this._busy = false
-      this._executeNext()
-    }
-  }
-
-  /**
-   * Add a task to the execution queue
-   */
-
-  private async _enqueue<T>(task: QueueTask): Promise<T> {
-    if (this._closed) {
-      const err = new Error('Connection is closed')
-      task.fail(err)
-      return Promise.reject(err)
-    }
-
-    // Create a promise if not already defined
-    if (!task.promise) {
-      task.promise = new Promise<T>((resolve, reject) => {
-        const originalExecute = task.execute
-        const originalFail = task.fail
-
-        // Override execute to resolve the promise
-        task.execute = (done) => {
-          originalExecute(() => {
-            resolve(undefined as any)
-            done()
-          })
-        }
-
-        // Override fail to reject the promise
-        task.fail = (err) => {
-          originalFail(err)
-          reject(err)
-        }
-      })
-    }
-
-    this._queue.push(task)
-    this._executeNext()
-    return task.promise as Promise<T>
-  }
-}
 
 class QueryResult {
 }
@@ -111,13 +18,13 @@ export class Connection extends EventEmitter {
   private readonly _queue: AsyncQueue = new AsyncQueue()
   private _closed = false
   public readonly _connectionId = 1
-  private readonly _logger: Logger = Logger.getInstance()
+  private readonly _logger: ClassLogger
 
   constructor (nativeConnection: NativeConnection) {
     super()
     this._native = nativeConnection
-    this._logger.setContextProvider(() => this._getLogContext())
-    // Create the promise-based API
+    this._logger = new ClassLogger('Connection', { connectionId: this._connectionId })
+    this._logger.debug('Connection ctor')
   }
 
   /**
@@ -126,12 +33,6 @@ export class Connection extends EventEmitter {
    */
   async enqueueTask<T>(task: QueueTask): Promise<T> {
     return this._queue.enqueueTask(task)
-  }
-
-  private _getLogContext (): Record<string, any> {
-    return {
-      connectionId: this._connectionId
-    }
   }
 
   /**
