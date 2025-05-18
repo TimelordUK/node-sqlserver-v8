4d30d0e81a5461c9c45b26077eda0ec732083e68
TimelordUK <sjames8888@gmail.com>
2025-05-18 20:45:38 +0100

    add type name mapper

---
+++ cpp/include/odbc/odbc_type_mapper.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <Logger.h>
+
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+#include "datum_storage.h"
+#include "odbc_driver_types.h"
+
+namespace mssql {
+
+class OdbcTypeMapper {
+ public:
+  static SQLSMALLINT parseOdbcParamTypeString(const std::string& typeStr);
+  static SQLSMALLINT parseOdbcTypeString(const std::string& typeStr);
+  static SQLSMALLINT getOdbcCTypeFromJsType(int jsType);
+  static SQLSMALLINT getOdbcSqlTypeFromJsType(int jsType);
+};
+}  // namespace mssql
